ammarfaizi2/public-inbox

commit b44af5d8f2fb106bee9b4720bcf92aed7851bfa8

git-http-backend: start refactoring to use callback
Designing for asynchronous, non-blocking operations makes
adapting for synchronous, blocking operation easy.

Going the other way around is not easy, so do it now and
allow us to be more easily adapted for non-blocking use
in the next commit...

lib/PublicInbox/GitHTTPBackend.pm
@@ -178,34 +178,55 @@ sub serve_smart {
 		my @cmd = qw(git http-backend);
 		exec(@cmd) or die 'exec `' . join(' ', @cmd). "' failed: $!\n";
 	}
-	$wpipe = undef;
-	$in = undef;
-	my @h;
-	my $code = 200;
-	{
-		local $/ = "\r\n";
-		while (defined(my $line = <$rpipe>)) {
-			if ($line =~ /\AStatus:\s*(\d+)/) {
-				$code = $1;
-			} else {
-				chomp $line;
-				last if $line eq '';
-				push @h, split(/:\s*/, $line, 2);
-			}
+	$wpipe = $in = undef;
+	$rpipe->blocking(0);
+	$buf = '';
+	my $vin;
+	vec($vin, fileno($rpipe), 1) = 1;
+	my ($fh, $res);
+	my $fail = sub {
+		my ($e) = @_;
+		if ($e eq 'EAGAIN') {
+			select($vin, undef, undef, undef);
+		} else {
+			$rpipe = undef;
+			$fh->close if $fh;
+			$err->print('git http-backend error: ', $e, "\n");
 		}
-	}
-	return if $code == 403;
-	sub {
-		my ($cb) = @_;
-		my $fh = $cb->([ $code, \@h ]);
-		while (1) {
-			my $r = sysread($rpipe, $buf, 8192);
-			die "$!\n" unless defined $r;
-			last if ($r == 0);
+	};
+	my $cb = sub {
+		my $r = sysread($rpipe, $buf, 8192, length($buf));
+		return $fail->($!{EAGAIN} ? 'EAGAIN' : $!) unless defined $r;
+		if ($r == 0) { # EOF
+			$rpipe = undef;
+			$fh->close if $fh;
+			return;
+		}
+		if ($fh) { # stream body from git-http-backend to HTTP client
 			$fh->write($buf);
-		}
-		$fh->close;
-	}
+			$buf = '';
+		} elsif ($buf =~ s/\A(.*?)\r?\n\r?\n//s) { # parse headers
+			my $h = $1;
+			my $code = 200;
+			my @h;
+			foreach my $l (split(/\r?\n/, $h)) {
+				my ($k, $v) = split(/:\s*/, $l, 2);
+				if ($k =~ /\AStatus\z/i) {
+					$code = int($v);
+				} else {
+					push @h, $k, $v;
+				}
+			}
+			# write response header:
+			$fh = $res->([ $code, \@h ]);
+			$fh->write($buf);
+			$buf = '';
+		} # else { keep reading ... }
+	};
+	sub {
+		($res) = @_;
+		while ($rpipe) { $cb->() }
+	};
 }
 
 1;

commit 4e49ab58f0ea6b32e9610a990c5bfe21900dc73b

git-http-backend: fallback to dumb when smart is disabled
We broke this fallback when we became more callback driven
and a test will come in the next commit.

However, our dumb cloning is still subject to scheduling
monopolization when serving gigantic packs.  Scheduling
will be fixed at a later time.

Fixes: b44af5d8f2fb ("git-http-backend: start refactoring to use callback")

lib/PublicInbox/GitHTTPBackend.pm
@@ -34,8 +34,15 @@ sub serve {
 	if ($service =~ /\Agit-\w+-pack\z/ || $path =~ /\Agit-\w+-pack\z/) {
 		my $ok = serve_smart($cgi, $git, $path);
 		return $ok if $ok;
+		# fall through to dumb HTTP...
 	}
+	serve_dumb($cgi, $git, $path);
+}
 
+sub serve_dumb {
+	my ($cgi, $git, $path) = @_;
+
+	# serve dumb HTTP...
 	my $type;
 	if ($path =~ /\A(?:$BIN)\z/o) {
 		$type = 'application/octet-stream';
@@ -123,7 +130,6 @@ sub prepare_range {
 	($code, $len);
 }
 
-# returns undef if 403 so it falls back to dumb HTTP
 sub serve_smart {
 	my ($cgi, $git, $path) = @_;
 	my $env = $cgi->{env};
@@ -158,7 +164,8 @@ sub serve_smart {
 	my $git_dir = $git->{git_dir};
 	$env{GIT_HTTP_EXPORT_ALL} = '1';
 	$env{PATH_TRANSLATED} = "$git_dir/$path";
-	my %rdr = ( 0 => fileno($in), 1 => fileno($wpipe) );
+	my %rdr = ( 0 => fileno($in), 1 => fileno($wpipe),
+			2 => $git->err_begin );
 	my $pid = spawn([qw(git http-backend)], \%env, \%rdr);
 	unless (defined $pid) {
 		$err->print("error spawning: $!\n");
@@ -202,7 +209,8 @@ sub serve_smart {
 		if ($fh) { # stream body from git-http-backend to HTTP client
 			$fh->write($buf);
 			$buf = '';
-		} elsif ($buf =~ s/\A(.*?)\r\n\r\n//s) { # parse headers
+		} elsif (defined $res && $buf =~ s/\A(.*?)\r\n\r\n//s) {
+			# parse headers
 			my $h = $1;
 			my $code = 200;
 			my @h;
@@ -214,11 +222,23 @@ sub serve_smart {
 					push @h, $k, $v;
 				}
 			}
-			# write response header:
-			$fh = $res->([ $code, \@h ]);
-			$res = undef;
-			$fh->write($buf);
-			$buf = '';
+			# incredibly convoluted, ugh...
+			if ($code == 403) {
+				my $d = serve_dumb($cgi, $git, $path);
+				if (ref($d) eq 'ARRAY') { # 404
+					$res->($d);
+				} else {
+					$d->($res);
+				}
+				$res = undef;
+				$end->();
+			} else {
+				# write response header:
+				$fh = $res->([ $code, \@h ]);
+				$res = undef;
+				$fh->write($buf);
+				$buf = '';
+			}
 		} # else { keep reading ... }
 	};
 	if (my $async = $env->{'pi-httpd.async'}) {