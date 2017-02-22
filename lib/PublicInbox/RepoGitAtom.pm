ammarfaizi2/public-inbox

commit f6244586ba4f5a5e7575e1254be8c9bbe303fce9

repobrowse: switch to new URL format to avoid query strings
Query strings make endpoint caching more difficult since
they're order-independent.  They are also more likely lost
or truncated inadvertantly when copy+pasting, so try to
avoid them for default endpoints.

There's still some things which are broken and followup
commits will be needed to fix them.

lib/PublicInbox/RepoGitAtom.pm
@@ -46,7 +46,7 @@ sub flush_hdr ($$$) {
 	$$dst .= '</published>';
 	$$dst .= qq(<link\nrel="alternate"\ntype="text/html"\nhref=");
 	$$dst .= $url;
-	$$dst .= '/commit?id=';
+	$$dst .= '/commit/';
 
 	my $H = $hdr->{H};
 	$$dst .= $H;
@@ -67,7 +67,7 @@ sub git_atom_sed ($$) {
 	my $rel = $req->{relcmd};
 	my $repo_info = $req->{repo_info};
 	my $title = join('/', $repo_info->{repo}, @{$req->{extra}});
-	$title = utf8_html("$title, branch $req->{q}->{h}");
+	$title = utf8_html("$title, $req->{-tip}");
 	my $url = repo_root_url($self, $req);
 	my $hdr = {};
 	my $subtitle = $repo_info->desc_html;
@@ -76,7 +76,7 @@ sub git_atom_sed ($$) {
 		qq(<title>$title</title>) .
 		qq(<subtitle>$subtitle</subtitle>) .
 		qq(<link\nrel="alternate"\ntype="text/html"\nhref="$url"\n/>);
-	my ($plinks, $id, $ai);
+	my ($plinks, $ai);
 	my $end = '';
 	my $blines;
 	sub {
@@ -140,12 +140,11 @@ sub call_git_atom {
 
 	my $git = $repo_info->{git};
 	my $env = $req->{env};
-	my $q =$req->{'q'} = PublicInbox::RepoGitQuery->new($env);
-	my $h = $q->{h};
+	my $tip = $req->{-tip};
 	my $read_log = sub {
 		my $cmd = $git->cmd(qw(log --no-notes --no-color
 					--abbrev-commit), $git->abbrev,
-					$ATOM_FMT, "-$max", $h, '--');
+					$ATOM_FMT, "-$max", $tip, '--');
 		my $expath = $req->{expath};
 		push @$cmd, $expath if $expath ne '';
 		my $rdr = { 2 => $git->err_begin };
@@ -155,13 +154,13 @@ sub call_git_atom {
 
 	sub {
 		$env->{'qspawn.response'} = $_[0];
-		return $read_log->() if $h ne '';
+		return $read_log->() if $tip ne '';
 
 		my $cmd = $git->cmd(qw(symbolic-ref --short HEAD));
 		my $rdr = { 2 => $git->err_begin };
 		my $qsp = PublicInbox::Qspawn->new($cmd, undef, undef, $rdr);
 		$qsp->psgi_qx($env, undef, sub {
-			chomp($h = ${$_[0]});
+			chomp($tip = ${$_[0]});
 			$read_log->();
 		})
 	}

lib/PublicInbox/RepoGitCommit.pm
@@ -37,11 +37,9 @@ sub commit_header {
 	my @p = split(' ', $p);
 
 	my $rel = $req->{relcmd};
-	my $q = $req->{'q'};
-	my $qs = $req->{qs} = $q->qs(id => $h);
 	my $x = $self->html_start($req, $s) . "\n" .
-		qq(   commit $H (<a\nhref="${rel}patch$qs">patch</a>)\n) .
-		qq(     tree <a\nrel=nofollow\nhref="${rel}tree?id=$h">$t</a>);
+		qq(   commit $H (<a\nhref="${rel}patch/$h">patch</a>)\n) .
+		qq(     tree <a\nrel=nofollow\nhref="${rel}tree/$h">$t</a>);
 
 	my $git = $req->{repo_info}->{git};
 	# extra show path information, if any
@@ -56,7 +54,7 @@ sub commit_header {
 			my $e = PublicInbox::Hval->utf8($_, join('/', @t));
 			$ep = $e->as_path;
 			my $eh = $e->as_html;
-			$ep = "${rel}tree/$ep?id=$h";
+			$ep = "${rel}tree/$ep/$h";
 			qq(<a\nrel=nofollow\nhref="$ep">$eh</a>);
 		} @$extra);
 		$path = "/$ep";
@@ -66,10 +64,10 @@ sub commit_header {
 	my $np = scalar @p;
 	if ($np == 1) {
 		my $p = $p[0];
-		$x .= git_parent_line('   parent', $p, $q, $git, $rel, $path);
+		$x .= git_parent_line('   parent', $p, $git, $rel);
 	} elsif ($np > 1) {
 		$req->{mhelp} = CC_MERGE;
-		my @common = ($q, $git, $rel, $path);
+		my @common = ($git, $rel);
 		my @t = @p;
 		my $p = shift @t;
 		$x .= git_parent_line('  parents', $p, @common);
@@ -119,24 +117,20 @@ sub git_commit_sed ($$) {
 sub call_git_commit { # RepoBase calls this
 	my ($self, $req) = @_;
 	my $env = $req->{env};
-	my $q = PublicInbox::RepoGitQuery->new($env);
-	my $id = $q->{id};
-	$id eq '' and $id = 'HEAD';
+	my $tip = $req->{-tip};
 
 	my $expath = $req->{expath};
 	if ($expath ne '') {
 		my $relup = join('', map { '../' } @{$req->{extra}});
-		my $qs = $q->qs;
-		return $self->r(301, $req, "$relup$qs#".to_attr($expath));
+		return $self->r(301, $req, "$relup#".to_attr($expath));
 	}
 
 	my $git = $req->{repo_info}->{git};
 	my $cmd = $git->cmd(qw(show -z --numstat -p --encoding=UTF-8
 			--no-notes --no-color -c),
-			$git->abbrev, GIT_FMT, $id, '--');
+			$git->abbrev, GIT_FMT, $tip, '--');
 	my $rdr = { 2 => $git->err_begin };
 	my $qsp = PublicInbox::Qspawn->new($cmd, undef, $rdr);
-	$req->{'q'} = $q;
 	$env->{'qspawn.quiet'} = 1;
 	$qsp->psgi_return($env, undef, sub { # parse header
 		my ($r, $bref) = @_;
@@ -159,8 +153,7 @@ sub git_commit_404 {
 
 	my $try = 'try';
 	$x = "<html><head><title>$x</title></head><body><pre><b>$x</b>\n\n";
-	my $qs = $req->{'q'}->qs(id => '');
-	$x .= "<a\nhref=\"$pfx$qs\">$try the latest commit in HEAD</a>\n";
+	$x .= "<a\nhref=\"$pfx\">$try the latest commit in HEAD</a>\n";
 	$x .= '</pre></body>';
 
 	[404, ['Content-Type'=>'text/html'], [ $x ]];
@@ -168,11 +161,10 @@ sub git_commit_404 {
 
 # FIXME: horrifically expensive...
 sub git_parent_line {
-	my ($pfx, $p, $q, $git, $rel, $path) = @_;
-	my $qs = $q->qs(id => $p);
+	my ($pfx, $p, $git, $rel) = @_;
 	my $t = git_commit_title($git, $p);
 	$t = defined $t ? utf8_html($t) : '';
-	$pfx . qq( <a\nid=P\nhref="${rel}commit$path$qs">$p</a> $t\n);
+	$pfx . qq( <a\nid=P\nhref="${rel}commit/$p">$p</a> $t\n);
 }
 
 # do not break anchor links if the combined diff doesn't show changes:
@@ -186,14 +178,13 @@ sub show_unchanged {
 		qq( See the <a\nhref="#P">parents</a>, ) .
 		"or view final state(s) below:\n\n";
 	my $rel = $req->{relcmd};
-	my $qs = $req->{qs};
 	foreach my $anchor (@unchanged) {
 		my $fn = $anchors->{$anchor};
 		my $p = PublicInbox::Hval->utf8(git_unquote($fn));
 		$p = $p->as_path;
 		$fn = utf8_html($fn);
 		$$dst .= qq(\t<a\nrel=nofollow);
-		$$dst .= qq(\nid="$anchor"\nhref="${rel}tree/$p$qs">);
+		$$dst .= qq(\nid="$anchor"\nhref="${rel}tree/$p">);
 		$$dst .= "$fn</a>\n";
 	}
 }

lib/PublicInbox/RepoGitDiff.pm
@@ -34,11 +34,8 @@ sub git_diff_sed ($$) {
 
 sub call_git_diff {
 	my ($self, $req) = @_;
+	my ($id, $id2) = split(/\.\./, $req->{h});
 	my $env = $req->{env};
-	my $q = PublicInbox::RepoGitQuery->new($env);
-	my $id = $q->{id};
-	my $id2 = $q->{id2};
-
 	my $git = $req->{repo_info}->{git};
 	my $cmd = $git->cmd(qw(diff-tree -z --numstat -p --encoding=UTF-8
 				--no-color -M -B -D -r), $id2, $id, '--');

lib/PublicInbox/RepoGitLog.pm
@@ -27,18 +27,18 @@ sub parent_links {
 
 sub flush_log_hdr ($$$) {
 	my ($req, $dst, $hdr) = @_;
-	my $rel = $req->{relcmd};
+	my $lpfx = $req->{lpfx};
 	my $seen = $req->{seen};
 	$$dst .= '<hr /><pre>' if scalar keys %$seen;
 	my $id = $hdr->{h};
 	$seen->{$id} = 1;
 	$$dst .= qq(<a\nid=p$id\n);
-	$$dst .= qq(href="${rel}commit?id=$id"><b>);
+	$$dst .= qq(href="${lpfx}commit/$id"><b>);
 	$$dst .= utf8_html($hdr->{'s'}); # FIXME may still OOM
 	$$dst .= '</b></a>';
 	my $D = $hdr->{D}; # FIXME: thousands of decorations may OOM us
 	if ($D ne '') {
-		$$dst .= ' (' . join(', ', git_dec_links($rel, $D)) . ')';
+		$$dst .= ' (' . join(', ', git_dec_links($lpfx, $D)) . ')';
 	}
 	my @p = split(/ /, $hdr->{p});
 	push @{$req->{parents}}, @p;
@@ -56,14 +56,14 @@ sub git_log_sed_end ($$) {
 	my $np = 0;
 	my $seen = $req->{seen};
 	my $git = $req->{repo_info}->{git};
-	my $rel = $req->{relcmd};
+	my $lpfx = $req->{lpfx};
 	foreach my $p (@{$req->{parents}}) {
 		next if $seen->{$p};
 		$seen->{$p} = ++$np;
 		my $s = git_commit_title($git, $p);
-		$m .= qq(\n<a\nid=p$p\nhref="?h=$p">$p</a>\t);
+		$m .= qq(\n<a\nid=p$p\nhref="$p">$p</a>\t);
 		$s = defined($s) ? utf8_html($s) : '';
-		$m .= qq(<a\nhref="${rel}commit?id=$p">$s</a>);
+		$m .= qq(<a\nhref="${lpfx}commit/$p">$s</a>);
 	}
 	if ($np == 0) {
 		$$dst .= "No commits follow";
@@ -123,17 +123,22 @@ sub call_git_log {
 	my ($self, $req) = @_;
 	my $repo_info = $req->{repo_info};
 	my $max = $repo_info->{max_commit_count} || 50;
+	my $h = $req->{h};
 	$max = int($max);
 	$max = 50 if $max == 0;
 	my $env = $req->{env};
-	my $q = $req->{'q'} = PublicInbox::RepoGitQuery->new($env);
-	my $h = $q->{h};
-	$h eq '' and $h = 'HEAD';
 	my $git = $repo_info->{git};
 	my $cmd = $git->cmd(qw(log --no-notes --no-color --abbrev-commit),
-				$git->abbrev, $LOG_FMT, "-$max", $h, '--');
+				$git->abbrev, $LOG_FMT, "-$max",
+				$req->{-tip}, '--');
 	my $rdr = { 2 => $git->err_begin };
-	my $title = "log: $repo_info->{repo} (" . utf8_html($h). ')';
+	my $title = "log: $repo_info->{repo}";
+	if (defined $h) {
+		$title .= ' ('. utf8_html($h). ')';
+		$req->{lpfx} = $req->{relcmd};
+	} else {
+		$req->{lpfx} = $req->{relcmd}.$req->{-tip};
+	}
 	$req->{lhtml} = $self->html_start($req, $title) . "\n\n";
 	my $qsp = PublicInbox::Qspawn->new($cmd, undef, $rdr);
 	$qsp->psgi_return($env, undef, sub {

lib/PublicInbox/RepoGitSnapshot.pm
@@ -36,9 +36,7 @@ our %FMT_TYPES = (
 sub call_git_snapshot ($$) { # invoked by PublicInbox::RepoBase::call
 	my ($self, $req) = @_;
 
-	my @extra = @{$req->{extra}};
-	my $ref = shift @extra;
-	return $self->r(404) if (!defined $ref) || scalar(@extra);
+	my $ref = $req->{-tip};
 	my $orig_fn = $ref;
 
 	# just in case git changes refname rules, don't allow wonky filenames

lib/PublicInbox/RepoGitTag.pm
@@ -19,9 +19,8 @@ my %cmd_map = ( # type => action
 sub call_git_tag {
 	my ($self, $req) = @_;
 
-	my $q = PublicInbox::RepoGitQuery->new($req->{env});
-	my $h = $q->{h};
-	$h eq '' and return git_tag_list($self, $req);
+	my $h = $req->{h};
+	defined $h or return git_tag_list($self, $req);
 	sub {
 		my ($res) = @_;
 		git_tag_show($self, $req, $h, $res);
@@ -58,7 +57,7 @@ sub git_show_tag_as_tag {
 	my $label = "$type $obj";
 	my $cmd = $cmd_map{$type} || 'show';
 	my $rel = $req->{relcmd};
-	my $obj_link = qq(<a\nhref="$rel$cmd?id=$obj">$label</a>);
+	my $obj_link = qq(<a\nhref="$rel$cmd/$obj">$label</a>);
 	$head = $h . "\n\n   tag <b>$tag</b>\nobject $obj_link\n";
 	if (my $tagger = $h{tagger}) {
 		$head .= 'tagger ' . join("\t", creator_split($tagger)) . "\n";
@@ -147,7 +146,7 @@ sub git_each_tag_sed ($$) {
 			my $h = $ref->as_html;
 			$ref = $ref->as_href;
 			$dst .= qq(<tr><td><tt>) .
-				qq(<a\nhref="?h=$ref"><b>$h</b></a>) .
+				qq(<a\nhref="tag/$ref"><b>$h</b></a>) .
 				qq(</tt></td><td><tt>$date</tt></td><td><tt>) .
 				utf8_html($s) . '</tt></td></tr>';
 		}
@@ -189,7 +188,7 @@ sub unknown_tag_type {
 	my $rel = $req->{relcmd};
 	my $label = "$type $hex";
 	my $cmd = $cmd_map{$type} || 'show';
-	my $obj_link = qq(<a\nhref="$rel$cmd?id=$hex">$label</a>\n);
+	my $obj_link = qq(<a\nhref="$rel$cmd/$hex">$label</a>\n);
 
 	$fh->write($self->html_start($req,
 				"$repo_info->{repo}: ref: $h") .

lib/PublicInbox/RepoGitTree.pm
@@ -21,14 +21,7 @@ sub call_git_tree {
 	my ($self, $req) = @_;
 	my @extra = @{$req->{extra}};
 	my $git = $req->{repo_info}->{git};
-	my $q = PublicInbox::RepoGitQuery->new($req->{env});
-	my $id = $q->{id};
-	if ($id eq '') {
-		chomp($id = $git->qx(qw(rev-parse --short=10 HEAD)));
-		$q->{id} = $id;
-	}
-
-	my $obj = "$id:$req->{expath}";
+	my $obj = "$req->{-tip}:$req->{expath}";
 	my ($hex, $type, $size) = $git->check($obj);
 
 	unless (defined($type)) {
@@ -41,7 +34,7 @@ sub call_git_tree {
 	if ($type eq 'tree') {
 		$opts->{noindex} = 1;
 		$req->{thtml} = $self->html_start($req, $title, $opts) . "\n";
-		git_tree_show($req, $hex, $q);
+		git_tree_show($req, $hex);
 	} elsif ($type eq 'blob') {
 		sub {
 			my $res = $_[0];
@@ -49,7 +42,7 @@ sub call_git_tree {
 				['Content-Type','text/html; charset=UTF-8']]);
 			$fh->write($self->html_start($req, $title, $opts) .
 					"\n");
-			git_blob_show($req, $fh, $git, $hex, $q);
+			git_blob_show($req, $fh, $git, $hex);
 			$fh->write('</body></html>');
 			$fh->close;
 		}
@@ -60,15 +53,15 @@ sub call_git_tree {
 }
 
 sub cur_path {
-	my ($req, $q) = @_;
-	my $qs = $q->qs;
+	my ($req) = @_;
 	my @ex = @{$req->{extra}} or return '<b>root</b>';
 	my $s;
 
+	my $tip = $req->{-tip};
 	my $rel = $req->{relcmd};
 	# avoid relative paths, here, we don't want to propagate
 	# trailing-slash URLs although we tolerate them
-	$s = "<a\nhref=\"${rel}tree$qs\">root</a>/";
+	$s = "<a\nhref=\"${rel}tree/$tip\">root</a>/";
 	my $cur = pop @ex;
 	my @t;
 	$s .= join('/', (map {
@@ -76,37 +69,38 @@ sub cur_path {
 		my $e = PublicInbox::Hval->utf8($_, join('/', @t));
 		my $ep = $e->as_path;
 		my $eh = $e->as_html;
-		"<a\nhref=\"${rel}tree/$ep$qs\">$eh</a>";
+		"<a\nhref=\"${rel}tree/$tip/$ep\">$eh</a>";
 	} @ex), '<b>'.utf8_html($cur).'</b>');
 }
 
 sub git_blob_show {
-	my ($req, $fh, $git, $hex, $q) = @_;
+	my ($req, $fh, $git, $hex) = @_;
 	# ref: buffer_is_binary in git.git
 	my $to_read = 8000; # git uses this size to detect binary files
 	my $text_p;
 	my $n = 0;
 
+	my $tip = $req->{-tip};
 	my $rel = $req->{relcmd};
-	my $plain = join('/', "${rel}plain", @{$req->{extra}});
-	$plain = PublicInbox::Hval->utf8($plain)->as_path . $q->qs;
-	my $t = cur_path($req, $q);
-	my $h = qq{\npath: $t\n\nblob $hex};
+	my $plain = join('/', "${rel}plain/$tip", @{$req->{extra}});
+	$plain = PublicInbox::Hval->utf8($plain)->as_path;
+	my $t = cur_path($req);
+	my $s = qq{\npath: $t\n\nblob $hex};
 	my $end = '';
 
 	$git->cat_file($hex, sub {
 		my ($cat, $left) = @_; # $$left == $size
-		$h .= qq{\t$$left bytes (<a\nhref="$plain">raw</a>)};
+		$s .= qq{\t$$left bytes (<a\nhref="$plain">raw</a>)};
 		$to_read = $$left if $to_read > $$left;
 		my $r = read($cat, my $buf, $to_read);
 		return unless defined($r) && $r > 0;
 		$$left -= $r;
 
 		if (index($buf, "\0") >= 0) {
-			$fh->write("$h\n$BINARY_MSG</pre>");
+			$fh->write("$s\n$BINARY_MSG</pre>");
 			return;
 		}
-		$fh->write($h."</pre><hr/><table\nsummary=blob><tr><td><pre>");
+		$fh->write($s."</pre><hr/><table\nsummary=blob><tr><td><pre>");
 		$text_p = 1;
 
 		while (1) {
@@ -147,7 +141,6 @@ sub git_tree_sed ($) {
 	my ($req) = @_;
 	my @lines;
 	my $buf = '';
-	my $qs = $req->{qs};
 	my $pfx = $req->{tpfx};
 	my $end;
 	sub {
@@ -180,29 +173,30 @@ sub git_tree_sed ($) {
 			# 'plain' and 'log' links intentionally omitted
 			# for brevity and speed
 			$dst .= qq($m\t).
-				qq($s\t<a\nhref="$pfx$ref$qs">$path</a>\n);
+				qq($s\t<a\nhref="$pfx/$ref">$path</a>\n);
 		}
 		$dst;
 	}
 }
 
 sub git_tree_show {
-	my ($req, $hex, $q) = @_;
+	my ($req, $hex) = @_;
 	my $git = $req->{repo_info}->{git};
 	my $cmd = $git->cmd(qw(ls-tree -l -z), $git->abbrev, $hex);
 	my $rdr = { 2 => $git->err_begin };
 	my $qsp = PublicInbox::Qspawn->new($cmd, undef, $rdr);
-	my $t = cur_path($req, $q);
+	my $t = cur_path($req);
 	my $pfx;
 
 	$req->{thtml} .= "\npath: $t\n\n<b>mode\tsize\tname</b>\n";
-	$req->{qs} = $q->qs;
 	if ($req->{tslash}) {
-		$pfx = './';
+		$pfx = '../';
 	} elsif (defined(my $last = $req->{extra}->[-1])) {
-		$pfx = PublicInbox::Hval->utf8($last)->as_path . '/';
+		$pfx = PublicInbox::Hval->utf8($last)->as_path;
+	} elsif (defined $req->{h}) {
+		$pfx = $req->{-tip};
 	} else {
-		$pfx = 'tree/';
+		$pfx = 'tree/' . $req->{-tip};
 	}
 	$req->{tpfx} = $pfx;
 	my $env = $req->{env};

lib/PublicInbox/Repobrowse.pm
@@ -89,7 +89,7 @@ sub call {
 	my $method = $env->{REQUEST_METHOD};
 	return r(405, 'Method Not Allowed') if ($method !~ /\AGET|HEAD|POST\z/);
 
-	# URL syntax: / repo [ / cmd [ / path ] ]
+	# URL syntax: / repo [ / cmd [ / head [ / path ] ] ]
 	# cmd: log | commit | diff | tree | view | blob | snapshot
 	# repo and path (@extra) may both contain '/'
 	my $path_info = $env->{PATH_INFO};
@@ -116,13 +116,16 @@ sub call {
 	my $vcs_lc = $repo_info->{vcs};
 	my $vcs = $VCS{$vcs_lc} or return r404();
 	my $mod;
+	my $h;
 	if (defined $cmd && length $cmd) {
 		$mod = $CMD{$cmd};
-		unless ($mod) {
+		if ($mod) {
+			$h = shift @extra if @extra;
+		} else {
 			unshift @extra, $cmd;
 			$mod = 'Fallback';
 		}
-		$req->{relcmd} = '../' x scalar(@extra);
+		$req->{relcmd} = '../' x (scalar(@extra) + 1);
 	} else {
 		$mod = 'Summary';
 		$cmd = 'summary';
@@ -137,7 +140,8 @@ sub call {
 		pop @extra;
 		++$tslash;
 	}
-
+	$req->{h} = $h;
+	$req->{-tip} = defined $h ? $h : 'HEAD';
 	return no_tslash($env) if ($tslash && $NO_TSLASH{$mod});
 
 	$req->{tslash} = $tslash;

commit 6158b3e3476cd49a2e6c32fad88a7905ec88de8f

repobrowse: fixup revision handling
Revisions passed in the URL must not be ignored.
This fixes some bugs introduced in commit
f6244586ba4f5a5e7575e1254be8c9bbe303fce9
("repobrowse: switch to new URL format to avoid query strings")

lib/PublicInbox/RepoGitAtom.pm
@@ -141,7 +141,7 @@ sub call_git_atom {
 
 	my $git = $repo->{git};
 	my $env = $req->{env};
-	my $tip = $req->{h} || $repo->tip;
+	my $tip = $req->{tip} || $repo->tip;
 	my $read_log = sub {
 		my $cmd = $git->cmd(qw(log --no-notes --no-color
 					--abbrev-commit), $git->abbrev,

lib/PublicInbox/RepoGitCommit.pm
@@ -124,11 +124,11 @@ sub call_git_commit { # RepoBase calls this
 		my $relup = join('', map { '../' } @{$req->{extra}});
 		return $self->r(301, $req, "$relup#".to_attr($expath));
 	}
-
+	my $tip = $req->{tip} || $req->{-repo}->tip;
 	my $git = $req->{-repo}->{git};
 	my $cmd = $git->cmd(qw(show -z --numstat -p --encoding=UTF-8
 			--no-notes --no-color -c --no-abbrev),
-			GIT_FMT, $req->{-repo}->tip, '--');
+			GIT_FMT, $tip, '--');
 	my $rdr = { 2 => $git->err_begin };
 	my $qsp = PublicInbox::Qspawn->new($cmd, undef, $rdr);
 	$env->{'qspawn.quiet'} = 1;

lib/PublicInbox/RepoGitDiff.pm
@@ -32,9 +32,10 @@ sub git_diff_sed ($$) {
 	}
 }
 
+# $REPO/diff/$BEFORE..$AFTER
 sub call_git_diff {
 	my ($self, $req) = @_;
-	my ($id, $id2) = split(/\.\./, $req->{h});
+	my ($id, $id2) = split(/\.\./, $req->{tip});
 	my $env = $req->{env};
 	my $git = $req->{-repo}->{git};
 	my $cmd = $git->cmd(qw(diff-tree -z --numstat -p --encoding=UTF-8

lib/PublicInbox/RepoGitLog.pm
@@ -124,22 +124,16 @@ sub call_git_log {
 	my ($self, $req) = @_;
 	my $repo = $req->{-repo};
 	my $max = $repo->{max_commit_count} || 50;
-	my $h = $req->{h};
+	my $tip = $req->{tip} || $repo->tip;
+	$req->{lpfx} = $req->{relcmd};
 	$max = int($max);
 	$max = 50 if $max == 0;
 	my $env = $req->{env};
 	my $git = $repo->{git};
-	my $tip = $req->{-repo}->tip;
 	my $cmd = $git->cmd(qw(log --no-notes --no-color --no-abbrev),
 				$LOG_FMT, "-$max", $tip, '--');
 	my $rdr = { 2 => $git->err_begin };
-	my $title = "log: $repo->{repo}";
-	if (defined $h) {
-		$title .= ' ('. utf8_html($h). ')';
-		$req->{lpfx} = $req->{relcmd};
-	} else {
-		$req->{lpfx} = $req->{relcmd} . $tip;
-	}
+	my $title = 'log: '.$repo->{repo}.' ('.utf8_html($tip).')';
 	$req->{lhtml} = $self->html_start($req, $title) . "\n\n";
 	my $qsp = PublicInbox::Qspawn->new($cmd, undef, $rdr);
 	$qsp->psgi_return($env, undef, sub {

lib/PublicInbox/RepoGitRaw.pm
@@ -12,7 +12,8 @@ sub call_git_raw {
 	my ($self, $req) = @_;
 	my $repo = $req->{-repo};
 	my $git = $repo->{git};
-	my $id = $repo->tip . ':' . $req->{expath};
+	my $tip = $req->{tip} || $repo->tip;
+	my $id = $tip . ':' . $req->{expath};
 	my ($cat, $hex, $type, $size) = $git->cat_file_begin($id);
 	return unless defined $cat;
 

lib/PublicInbox/RepoGitSnapshot.pm
@@ -36,7 +36,7 @@ our %FMT_TYPES = (
 sub call_git_snapshot ($$) { # invoked by PublicInbox::RepoBase::call
 	my ($self, $req) = @_;
 
-	my $ref = $req->{h} || $req->{-repo}->tip;
+	my $ref = $req->{tip} || $req->{-repo}->tip;
 	my $orig_fn = $ref;
 
 	# just in case git changes refname rules, don't allow wonky filenames

lib/PublicInbox/RepoGitTag.pm
@@ -19,11 +19,11 @@ my %cmd_map = ( # type => action
 sub call_git_tag {
 	my ($self, $req) = @_;
 
-	my $h = $req->{h};
-	defined $h or return git_tag_list($self, $req);
+	my $tip = $req->{tip};
+	defined $tip or return git_tag_list($self, $req);
 	sub {
 		my ($res) = @_;
-		git_tag_show($self, $req, $h, $res);
+		git_tag_show($self, $req, $tip, $res);
 	}
 }
 

lib/PublicInbox/RepoGitTree.pm
@@ -22,7 +22,7 @@ sub call_git_tree {
 	my @extra = @{$req->{extra}};
 	my $repo = $req->{-repo};
 	my $git = $repo->{git};
-	my $tip = $repo->tip;
+	my $tip = $req->{tip} || $repo->tip;
 	my $obj = "$tip:$req->{expath}";
 	my ($hex, $type, $size) = $git->check($obj);
 
@@ -31,8 +31,7 @@ sub call_git_tree {
 	}
 
 	my $opts = { nofollow => 1 };
-	my $title = $req->{expath};
-	$title = $title eq '' ? 'tree' : utf8_html($title);
+	my $title = "tree: ".utf8_html($req->{expath});
 	if ($type eq 'tree') {
 		$opts->{noindex} = 1;
 		$req->{thtml} = $self->html_start($req, $title, $opts) . "\n";
@@ -193,8 +192,8 @@ sub git_tree_show {
 	$req->{thtml} .= "\npath: $t\n\n<b>mode\tsize\tname</b>\n";
 	if (defined(my $last = $req->{extra}->[-1])) {
 		$pfx = PublicInbox::Hval->utf8($last)->as_path;
-	} elsif (defined $req->{h}) {
-		$pfx = $req->{-repo}->tip;
+	} elsif (defined(my $tip = $req->{tip})) {
+		$pfx = $tip;
 	} else {
 		$pfx = 'tree/' . $req->{-repo}->tip;
 	}

lib/PublicInbox/Repobrowse.pm
@@ -118,11 +118,11 @@ sub call {
 	my $vcs_lc = $repo->{vcs};
 	my $vcs = $VCS{$vcs_lc} or return r404();
 	my $mod;
-	my $h;
+	my $tip;
 	if (defined $cmd && length $cmd) {
 		$mod = $CMD{$cmd};
 		if ($mod) {
-			$h = shift @extra if @extra;
+			$tip = shift @extra if @extra;
 		} else {
 			unshift @extra, $cmd;
 			$mod = 'Fallback';
@@ -141,7 +141,7 @@ sub call {
 	while (@extra && $extra[-1] eq '') {
 		pop @extra;
 	}
-	$req->{h} = $h;
+	$req->{tip} = $tip;
 	$mod = load_once("PublicInbox::Repo$vcs$mod");
 	$vcs = load_once("PublicInbox::$vcs");
 