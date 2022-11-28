ammarfaizi2/public-inbox

commit 0c62cffc23894a2f5eb1d6c9ee29973f4863a365

nntp: listgroup_range_i: remove useless `map' op
No need to iterate through the array twice; and this even seems
a hair faster than what I got with commit 726d6e71aee5d974
(nntp: small speed up for multi-line responses, 2020-12-04)

lib/PublicInbox/NNTP.pm
@@ -189,7 +189,7 @@ sub listgroup_range_i {
 	my ($self, $beg, $end) = @_;
 	my $r = $self->{ibx}->mm(1)->msg_range($beg, $end, 'num');
 	scalar(@$r) or return;
-	$self->msg_more(join('', map { "$_->[0]\r\n" } @$r));
+	$self->msg_more(join("\r\n", @$r, ''));
 	1;
 }
 

commit b66f13a1764606b9c4d0a6a8e22ab1a8e30570ad

nntpd: fix LISTGROUP with range
This reverts 0c62cffc2389 ("nntp: listgroup_range_i: remove useless
`map' op") and adds a test that demonstrates the breakage: the server
returns lines like

    ARRAY(0x556dace73f08)

instead of message numbers.

Fixes: 0c62cffc2389 ("nntp: listgroup_range_i: remove useless `map' op")

lib/PublicInbox/NNTP.pm
@@ -188,7 +188,7 @@ sub listgroup_range_i {
 	my ($self, $beg, $end) = @_;
 	my $r = $self->{ibx}->mm(1)->msg_range($beg, $end, 'num');
 	scalar(@$r) or return;
-	$self->msg_more(join("\r\n", @$r, ''));
+	$self->msg_more(join('', map { "$_->[0]\r\n" } @$r));
 	1;
 }
 

t/nntpd.t
@@ -93,6 +93,13 @@ close $cfgfh or BAIL_OUT;
 	is_deeply([$n->group($group)], [ qw(0 1 1), $group ], 'GROUP works');
 	is_deeply($n->listgroup($group), [1], 'listgroup OK');
 	# TODO: Net::NNTP::listgroup does not support range at the moment
+	my $s = tcp_connect($sock);
+	sysread($s, my $buf, 4096);
+	is($buf, "201 " . hostname . " ready - post via email\r\n",
+		'got greeting');
+	syswrite($s, "LISTGROUP $group 1-1\r\n");
+	$buf = read_til_dot($s);
+	like($buf, qr/\r\n1\r\n/s, 'LISTGROUP with range works');
 
 	{
 		my $expect = [ qw(Subject: From: Date: Message-ID:
@@ -120,8 +127,8 @@ close $cfgfh or BAIL_OUT;
 		'references' => '<reftabsqueezed>',
 	);
 
-	my $s = tcp_connect($sock);
-	sysread($s, my $buf, 4096);
+	$s = tcp_connect($sock);
+	sysread($s, $buf, 4096);
 	is($buf, "201 " . hostname . " ready - post via email\r\n",
 		'got greeting');
 