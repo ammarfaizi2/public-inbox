f089fec3d66d18dde6aebb0740a7232b0cd571bf
Eric Wong <e@80x24.org>
2022-12-24 07:17:07 +0000

    spawn_pp: cleanup, error checks and descriptive errors
    The pipe(2) call needs to be checked for failure.  While we're
    at it, none of this is affected by unicode_strings, so Perl v5.12
    is safe to use and gets rid of the strict.pm overhead.
    
    We can also `die' directly since it's pure Perl and not contort
    our Perl code to the assumptions of the Inline::C version.
    
    `die' already implies a failure, so follow existing conventions
    of just having the failing function or op name.
    
    We can also rely on the grep op for filtering out non-system
    signals to avoid writing a loop ourselves.
    
    Finally, drop a needless `undef' on the read side of the pipe
    since it's already closed immediately in the child.

--- lib/PublicInbox/SpawnPP.pm
+++ lib/PublicInbox/SpawnPP.pm
@@ -1,11 +1,10 @@
-# Copyright (C) 2016-2021 all contributors <meta@public-inbox.org>
+# Copyright (C) all contributors <meta@public-inbox.org>
 # License: AGPL-3.0+ <https://www.gnu.org/licenses/agpl-3.0.txt>
 
 # Pure-Perl implementation of "spawn".  This can't take advantage
 # of vfork, so no speedups under Linux for spawning from large processes.
 package PublicInbox::SpawnPP;
-use strict;
-use v5.10.1;
+use v5.12;
 use POSIX qw(dup2 _exit setpgid :signal_h);
 
 # Pure Perl implementation for folks that do not use Inline::C
@@ -13,15 +12,11 @@ sub pi_fork_exec ($$$$$$$) {
 	my ($redir, $f, $cmd, $env, $rlim, $cd, $pgid) = @_;
 	my $old = POSIX::SigSet->new();
 	my $set = POSIX::SigSet->new();
-	$set->fillset or die "fillset failed: $!";
-	sigprocmask(SIG_SETMASK, $set, $old) or die "can't block signals: $!";
+	$set->fillset or die "sigfillset: $!";
+	sigprocmask(SIG_SETMASK, $set, $old) or die "SIG_SETMASK(set): $!";
 	my $syserr;
-	pipe(my ($r, $w));
-	my $pid = fork;
-	unless (defined $pid) { # compat with Inline::C version
-		$syserr = $!;
-		$pid = -1;
-	}
+	pipe(my ($r, $w)) or die "pipe: $!";
+	my $pid = fork // die "fork (+exec) @$cmd: $!\n";
 	if ($pid == 0) {
 		close $r;
 		$SIG{__DIE__} = sub {
@@ -38,9 +33,7 @@ sub pi_fork_exec ($$$$$$$) {
 		if ($pgid >= 0 && !defined(setpgid(0, $pgid))) {
 			die "setpgid(0, $pgid): $!";
 		}
-		for (keys %SIG) {
-			$SIG{$_} = 'DEFAULT' if substr($_, 0, 1) ne '_';
-		}
+		$SIG{$_} = 'DEFAULT' for grep(!/\A__/, keys %SIG);
 		if ($cd ne '') {
 			chdir $cd or die "chdir $cd: $!";
 		}
@@ -49,25 +42,22 @@ sub pi_fork_exec ($$$$$$$) {
 			BSD::Resource::setrlimit($r, $soft, $hard) or
 				die "setrlimit($r=[$soft,$hard]: $!)";
 		}
-		$old->delset(POSIX::SIGCHLD) or die "delset SIGCHLD: $!";
-		sigprocmask(SIG_SETMASK, $old) or die "SETMASK: ~SIGCHLD: $!";
+		$old->delset(POSIX::SIGCHLD) or die "sigdelset CHLD: $!";
+		sigprocmask(SIG_SETMASK, $old) or die "SIG_SETMASK ~CHLD: $!";
 		$cmd->[0] = $f;
 		if ($ENV{MOD_PERL}) {
 			@$cmd = (which('env'), '-i', @$env, @$cmd);
 		} else {
 			%ENV = map { split(/=/, $_, 2) } @$env;
 		}
-		undef $r;
 		exec { $f } @$cmd;
 		die "exec @$cmd failed: $!";
 	}
 	close $w;
-	sigprocmask(SIG_SETMASK, $old) or die "can't unblock signals: $!";
+	sigprocmask(SIG_SETMASK, $old) or die "SIG_SETMASK(old): $!";
 	if (my $cerrnum = do { local $/, <$r> }) {
-		$pid = -1;
 		$! = $cerrnum;
-	} else {
-		$! = $syserr;
+		die "forked child $@: $!";
 	}
 	$pid;
 }
