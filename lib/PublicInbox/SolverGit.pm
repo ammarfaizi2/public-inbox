ammarfaizi2/public-inbox

commit 6f5b238bae5c88b2b9b2fdf7834e17e78a33abd2

solver: early make hints detection more robust
Hints fields can change, so we'll use a simple boolean rather
than checking a static count.  We'll also short-circuit out
reliably regardless of hints when a full OID is given.

lib/PublicInbox/SolverGit.pm
@@ -81,9 +81,8 @@ sub solve_existing ($$) {
 	my $oid_b = $want->{oid_b};
 	my ($oid_full, $type, $size) = $git->check($oid_b);
 
-	# other than {oid_b, try_gits, try_ibxs}
-	my $have_hints = scalar keys %$want > 3;
-	if (defined($type) && (!$have_hints || $type eq 'blob')) {
+	if ($oid_b eq ($oid_full // '') || (defined($type) &&
+-				(!$self->{have_hints} || $type eq 'blob'))) {
 		delete $want->{try_gits};
 		return [ $git, $oid_full, $type, int($size) ]; # done, success
 	}
@@ -683,6 +682,7 @@ sub solve ($$$$$) {
 	$self->{seen_oid} = {};
 	$self->{tot} = 0;
 	$self->{psgi_env} = $env;
+	$self->{have_hints} = 1 if scalar keys %$hints;
 	$self->{todo} = [ { %$hints, oid_b => $oid_want } ];
 	$self->{patches} = []; # [ $di, $di, ... ]
 	$self->{found} = {}; # { abbr => [ ::Git, oid, type, size, $di ] }

commit 3c997cf32dea4a19d1c16e4c17ef974a3a647576

solver_git: remove extraneous leading `-'
It was a harmless negation, I must've pasted a line from a diff
and forgotten to chop off the first character :x

Fixes: 6f5b238bae5c "solver: early make hints detection more robust"

lib/PublicInbox/SolverGit.pm
@@ -82,7 +82,7 @@ sub solve_existing ($$) {
 	my ($oid_full, $type, $size) = $git->check($oid_b);
 
 	if ($oid_b eq ($oid_full // '') || (defined($type) &&
--				(!$self->{have_hints} || $type eq 'blob'))) {
+				(!$self->{have_hints} || $type eq 'blob'))) {
 		delete $want->{try_gits};
 		return [ $git, $oid_full, $type, int($size) ]; # done, success
 	}