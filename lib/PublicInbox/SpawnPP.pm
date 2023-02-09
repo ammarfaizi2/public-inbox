ammarfaizi2/public-inbox

commit 9eb8baf199cd148b7ebf8e6e130fb832f4e1ef00

spawn_pp: use `which()' properly for pure-Perl spawn
I have no idea if mod_perl/mod_perl2 is used nowadays, but
we're stuck supporting it as long as mod_perl exists.  So
add some tests and make minor updates to existing ones to
ensure it stays working.

lib/PublicInbox/SpawnPP.pm
@@ -3,9 +3,11 @@
 
 # Pure-Perl implementation of "spawn".  This can't take advantage
 # of vfork, so no speedups under Linux for spawning from large processes.
+# Do not require this directly, only use from PublicInbox::Spawn
 package PublicInbox::SpawnPP;
 use v5.12;
 use POSIX qw(dup2 _exit setpgid :signal_h);
+use PublicInbox::Spawn qw(which);
 
 # Pure Perl implementation for folks that do not use Inline::C
 sub pi_fork_exec ($$$$$$$) {
@@ -46,7 +48,8 @@ sub pi_fork_exec ($$$$$$$) {
 		sigprocmask(SIG_SETMASK, $old) or die "SIG_SETMASK ~CHLD: $!";
 		$cmd->[0] = $f;
 		if ($ENV{MOD_PERL}) {
-			@$cmd = (which('env'), '-i', @$env, @$cmd);
+			$f = which('env');
+			@$cmd = ('env', '-i', @$env, @$cmd);
 		} else {
 			%ENV = map { split(/=/, $_, 2) } @$env;
 		}

t/spawn.t
@@ -1,10 +1,11 @@
-# Copyright (C) 2015-2021 all contributors <meta@public-inbox.org>
+#!perl -w
+# Copyright (C) all contributors <meta@public-inbox.org>
 # License: AGPL-3.0+ <https://www.gnu.org/licenses/agpl-3.0.txt>
-use strict;
-use warnings;
+use v5.12;
 use Test::More;
 use PublicInbox::Spawn qw(which spawn popen_rd);
-use PublicInbox::Sigfd;
+require PublicInbox::Sigfd;
+require PublicInbox::DS;
 
 {
 	my $true = which('true');
@@ -38,9 +39,8 @@ SKIP: {
 	$pid = eval { spawn(['true'], undef, { pgid => $wrong_pgid, 2 => $w }) };
 	close $w;
 	my $err = do { local $/; <$r> };
-	# diag "$err ($@)";
 	if (defined $pid) {
-		waitpid($pid, 0) if defined $pid;
+		waitpid($pid, 0);
 		isnt($?, 0, 'child error (pure-Perl)');
 	} else {
 		ok($@, 'exception raised');
@@ -65,7 +65,7 @@ EOF
 	my $rd = popen_rd([$^X, '-e', $script]);
 	diag 'waiting for child to reap grandchild...';
 	chomp(my $line = readline($rd));
-	my ($rdy, $pid) = split(' ', $line);
+	my ($rdy, $pid) = split(/ /, $line);
 	is($rdy, 'RDY', 'got ready signal, waitpid(-1) works in child');
 	ok(kill('CHLD', $pid), 'sent SIGCHLD to child');
 	is(readline($rd), "HI\n", '$SIG{CHLD} works in child');
@@ -199,6 +199,30 @@ SKIP: {
 	isnt($?, 0, 'non-zero exit status');
 }
 
-done_testing();
+SKIP: {
+	require PublicInbox::SpawnPP;
+	require File::Temp;
+	my $tmp = File::Temp->newdir('spawnpp-XXXX', TMPDIR => 1);
+	my $cmd = [ qw(/bin/sh -c), 'echo $HI >foo' ];
+	my $env = [ 'HI=hihi' ];
+	my $rlim = [];
+	my $pgid = -1;
+	my $pid = PublicInbox::SpawnPP::pi_fork_exec([], '/bin/sh', $cmd, $env,
+						$rlim, "$tmp", $pgid);
+	is(waitpid($pid, 0), $pid, 'spawned process exited');
+	is($?, 0, 'no error');
+	open my $fh, '<', "$tmp/foo" or die "open: $!";
+	is(readline($fh), "hihi\n", 'env+chdir worked for SpawnPP');
+	close $fh;
+	unlink("$tmp/foo") or die "unlink: $!";
+	{
+		local $ENV{MOD_PERL} = 1;
+		$pid = PublicInbox::SpawnPP::pi_fork_exec([],
+				'/bin/sh', $cmd, $env, $rlim, "$tmp", $pgid);
+	}
+	is(waitpid($pid, 0), $pid, 'spawned process exited');
+	open $fh, '<', "$tmp/foo" or die "open: $!";
+	is(readline($fh), "hihi\n", 'env+chdir SpawnPP under (faked) MOD_PERL');
+}
 
-1;
+done_testing();

commit 5541df4f6dbf7d58590a65cff39288e6005228b0

spawn_pp: fix incorrect `use'
We can't `use PublicInbox::Spawn' from SpawnPP because
PublicInbox::Spawn loads SpawnPP from BEGIN.

Fixes: 9eb8baf199cd148b (spawn_pp: use `which()' properly for pure-Perl spawn, 2023-01-29)

lib/PublicInbox/SpawnPP.pm
@@ -7,7 +7,7 @@
 package PublicInbox::SpawnPP;
 use v5.12;
 use POSIX qw(dup2 _exit setpgid :signal_h);
-use PublicInbox::Spawn qw(which);
+# this is loaded by PublicInbox::Spawn, so we can't use/require it, here
 
 # Pure Perl implementation for folks that do not use Inline::C
 sub pi_fork_exec ($$$$$$$) {
@@ -48,7 +48,7 @@ sub pi_fork_exec ($$$$$$$) {
 		sigprocmask(SIG_SETMASK, $old) or die "SIG_SETMASK ~CHLD: $!";
 		$cmd->[0] = $f;
 		if ($ENV{MOD_PERL}) {
-			$f = which('env');
+			$f = PublicInbox::Spawn::which('env');
 			@$cmd = ('env', '-i', @$env, @$cmd);
 		} else {
 			%ENV = map { split(/=/, $_, 2) } @$env;