ammarfaizi2/public-inbox

commit 836faf5093dfec84aa3d1a0abd5c546e02b46541

lei_mirror: add `index' target to generated Makefile
It can probably be a useful hint to avoid misleading users
into always using `--reindex'.

lib/PublicInbox/LeiMirror.pm
@@ -763,6 +763,7 @@ help :
 	@echo Rarely needed targets:
 	@echo '    make reindex      - may be needed for new features/bugfixes'
 	@echo '    make compact      - rewrite Xapian storage to save space'
+	@echo '    make index        - initial index after clone
 
 fetch :
 	public-inbox-fetch
@@ -779,12 +780,14 @@ update :
 		echo 'public-inbox index not initialized'; \
 		echo 'see public-inbox-index(1) man page'; \
 	fi
+index :
+	public-inbox-index
 reindex :
 	public-inbox-index --reindex
 compact :
 	public-inbox-compact
 
-.PHONY : help fetch update reindex compact
+.PHONY : help fetch update index reindex compact
 EOM
 		close $fh or die "close($f): $!";
 	} else {

commit 266766fad33f6ffd6d4c8b285b7947fdfcce9c97

lei_mirror: fix sh syntax error in "make help" target
Oops :x

Fixes: 836faf5093df ("lei_mirror: add `index' target to generated Makefile")

lib/PublicInbox/LeiMirror.pm
@@ -1370,7 +1370,7 @@ help :
 	@echo Rarely needed targets:
 	@echo '    make reindex      - may be needed for new features/bugfixes'
 	@echo '    make compact      - rewrite Xapian storage to save space'
-	@echo '    make index        - initial index after clone
+	@echo '    make index        - initial index after clone'
 
 fetch :
 	public-inbox-fetch

t/lei-mirror.t
@@ -1,10 +1,11 @@
 #!perl -w
-# Copyright (C) 2020-2021 all contributors <meta@public-inbox.org>
+# Copyright (C) all contributors <meta@public-inbox.org>
 # License: AGPL-3.0+ <https://www.gnu.org/licenses/agpl-3.0.txt>
 use strict; use v5.10.1; use PublicInbox::TestCommon;
 use PublicInbox::Inbox;
 require_mods(qw(-httpd lei DBD::SQLite));
 require_cmd('curl');
+use PublicInbox::Spawn qw(which);
 require PublicInbox::Msgmap;
 my $sock = tcp_server();
 my ($tmpdir, $for_destroy) = tmpdir();
@@ -25,7 +26,10 @@ test_lei({ tmpdir => $tmpdir }, sub {
 	is(PublicInbox::Git::try_cat("$t1/description"),
 		"mirror of $http/t1/\n", 'description set');
 	ok(-f "$t1/Makefile", 'convenience Makefile added (v1)');
+	my $make = which('make');
+	xsys_e([$make, 'help'], undef, { -C => $t1, 1 => \(my $help) });
 	ok(-f "$t1/inbox.config.example", 'inbox.config.example downloaded');
+	isnt($help, '', 'make help worked');
 	is((stat(_))[9], $created{v1},
 		'inbox.config.example mtime is ->created_at');
 	is((stat(_))[2] & 0222, 0, 'inbox.config.example not writable');