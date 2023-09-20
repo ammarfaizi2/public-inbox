ammarfaizi2/public-inbox

commit 82990fb72dac23ccc8d7c0d8388151514b3c0366

install/deps: flesh out libgit2, SQLite, and Xapian packages
This seems to work reasonably well across multiple BSDs and
Debian and smooth out differences in packaging granularity.

Declaring the $always_deps relationship between `Xapian'(.pm) and
libxapian means we can get rid of the OpenBSD-specific hack to
remove `xapian-bindings-perl' before other packages.

install/deps.perl
@@ -50,7 +50,6 @@ my $profiles = {
 
 	# everything optional for normal use
 	optional => [ qw(
-		curl
 		Date::Parse
 		BSD::Resource
 		DBD::SQLite
@@ -62,7 +61,11 @@ my $profiles = {
 		Plack::Test
 		Plack::Middleware::ReverseProxy
 		Xapian
+		curl
 		highlight.pm
+		libxapian
+		pkg-config
+		sqlite3
 		xapian-tools
 		) ],
 
@@ -76,9 +79,14 @@ my $profiles = {
 
 # only for distro-agnostic dependencies which are always true:
 my $always_deps = {
-	'DBD::SQLite' => [ qw(DBI) ],
-	'Mail::IMAPClient' => [ qw(Parse::RecDescent) ],
-	'Plack::Middleware::ReverseProxy' => [ qw(Plack) ],
+	# we only load DBI explicitly
+	'DBD::SQLite' => [ qw(DBI libsqlite3) ],
+	'Mail::IMAPClient' => 'Parse::RecDescent',
+	'Plack::Middleware::ReverseProxy' => 'Plack',
+	'Xapian' => 'libxapian',
+	'xapian-tools' => 'libxapian',
+	'libxapian-dev' => [ qw(pkg-config libxapian) ],
+	'libgit2-dev' => 'pkg-config',
 };
 
 # bare minimum for v2
@@ -110,7 +118,7 @@ $profiles->{'watch-maildir'} = [ @{$profiles->{v2essential}} ];
 
 # package names which can't be mapped automatically and explicit
 # dependencies to prevent essential package removal:
-my $non_auto = {
+my $non_auto = { # git and perl are essential
 	git => {
 		pkg => [ qw(curl p5-TimeDate git) ],
 		rpm => [ qw(curl git) ],
@@ -121,6 +129,7 @@ my $non_auto = {
 		pkgin => 'perl',
 		pkg_add => [], # Perl is part of OpenBSD base
 	},
+	# optional stuff:
 	'Date::Parse' => {
 		deb => 'libtimedate-perl',
 		pkg => 'p5-TimeDate',
@@ -138,9 +147,8 @@ my $non_auto = {
 	},
 	'Xapian' => {
 		deb => 'libsearch-xapian-perl',
-		pkg => [qw(xapian-core p5-Xapian)],
-		pkg_add => [qw(xapian-core xapian-bindings-perl)],
-		pkgin => [qw(xapian p5-Xapian)],
+		pkg => 'p5-Xapian',
+		pkg_add => 'xapian-bindings-perl',
 		rpm => 'Search::Xapian', # 3rd-party repo
 	},
 	'highlight.pm' => {
@@ -148,6 +156,40 @@ my $non_auto = {
 		pkg => [],
 		pkgin => 'p5-highlight',
 		rpm => [],
+	},
+
+	# `libgit2' is the project name (since git has libgit)
+	'libgit2-dev' => {
+		pkg => 'libgit2',
+		rpm => 'libgit2-devel',
+	},
+
+	# some distros have both sqlite 2 and 3, we've only ever used 3
+	'libsqlite3' => {
+		pkg => 'sqlite3',
+		rpm => [], # `sqlite' is not removable due to yum/systemd
+		deb => [], # libsqlite3-0, but no need to specify
+	},
+
+	# only one version of Xapian distros
+	'libxapian' => { # avoid .so version numbers in our deps
+		deb => [], # libxapian30 atm, but no need to specify
+		pkg => 'xapian-core',
+		pkgin => 'xapian',
+		rpm => 'xapian-core',
+	},
+	'libxapian-dev' => {
+		pkg => 'xapian-core',
+		pkgin => 'xapian',
+		rpm => 'xapian-core-devel',
+	},
+	'pkg-config' => {
+		pkg_add => [], # part of the OpenBSD base system
+		pkg => 'pkgconf', # pkg-config is a symlink to pkgconf
+		pkgin => 'pkg-config',
+	},
+	'sqlite3' => { # this is just the executable binary on deb
+		rpm => [], # `sqlite' is not removable due to yum/systemd
 	},
 
 	# we call xapian-compact(1) in public-inbox-compact(1) and
@@ -205,7 +247,7 @@ for my $ary (values %$profiles) {
 	my @extra;
 	for my $pkg (@$ary) {
 		my $deps = $always_deps->{$pkg} // next;
-		push @extra, @$deps;
+		push @extra, list($deps);
 	}
 	push @$ary, @extra;
 	$all{$_} = \@pkg_remove for @$ary;
@@ -267,15 +309,6 @@ if ($pkg_fmt eq 'deb') {
 	root(qw(yum install), @pkg_opt, @pkg_install) if @pkg_install;
 } elsif ($pkg_fmt eq 'pkg_add') { # OpenBSD
 	my @pkg_opt = $opt->{yes} ? qw(-I) : (); # -I means non-interactive
-	if (@pkg_remove) {
-		my @lifo = qw(xapian-bindings-perl);
-		for my $dep (@lifo) {
-			grep(/\A\Q$dep\E\z/, @pkg_remove) or next;
-			root(qw(pkg_delete), @pkg_opt, $dep);
-			@pkg_remove = grep(!/\A\Q$dep\E\z/, @pkg_remove);
-		}
-		root(qw(pkg_delete), @pkg_opt, @pkg_remove);
-	}
 	root(qw(pkg_delete -a), @pkg_opt); # autoremove unspecified
 	@pkg_install = map { "$_--" } @pkg_install; # disambiguate w3m
 	root(qw(pkg_add), @pkg_opt, @pkg_install) if @pkg_install;

commit 79874c84bad7c0c720751bdcc1a8398a2e50f885

install/deps: more fixes
We need to mark *-dev packages as optional, and ignore
dependency-only packages in the `optional' field.

Furthermore, make the output less confusing when there's neither
packages to install nor remove; and avoid invoking `apt-get install'
with an empty package list.

This also fixes an OpenBSD-specific regression from commit 82990fb72dac
which made package removal a no-op.

Fixes: 82990fb72dac (install/deps: flesh out libgit2, SQLite, and Xapian packages)

install/deps.perl
@@ -48,7 +48,8 @@ my $profiles = {
 		URI
 		), @test_essential ],
 
-	# everything optional for normal use
+	# Everything else is optional for normal use.  Only specify
+	# the minimum to pull in dependencies here:
 	optional => [ qw(
 		Date::Parse
 		BSD::Resource
@@ -63,11 +64,14 @@ my $profiles = {
 		Xapian
 		curl
 		highlight.pm
-		libxapian
-		pkg-config
+		libgit2-dev
+		libxapian-dev
 		sqlite3
 		xapian-tools
 		) ],
+		# no pkg-config, libsqlite3, libxapian, libz, etc. since
+		# they'll get pulled in lib*-dev, DBD::SQlite and
+		# Xapian(.pm)  respectively
 
 	# optional developer stuff
 	devtest => [ qw(
@@ -279,6 +283,8 @@ my (%add, %rm); # uniquify lists
 		!$add{$_} && !$rm{$_}++ && $INST_CHECK->($_)
 	} @pkg_remove : ();
 
+(@pkg_remove || @pkg_install) or warn "# no packages to install nor remove\n";
+
 # OS-specific cleanups appreciated
 if ($pkg_fmt eq 'deb') {
 	my @apt_opt = qw(-o APT::Install-Recommends=false
@@ -288,7 +294,7 @@ if ($pkg_fmt eq 'deb') {
 		@pkg_install,
 		# apt-get lets you suffix a package with "-" to
 		# remove it in an "install" sub-command:
-		map { "$_-" } @pkg_remove);
+		map { "$_-" } @pkg_remove) if (@pkg_remove || @pkg_install);
 	root('apt-get', @apt_opt, qw(autoremove)) if $opt->{'allow-remove'};
 } elsif ($pkg_fmt eq 'pkg') { # FreeBSD
 	my @pkg_opt = $opt->{yes} ? qw(-y) : ();
@@ -309,9 +315,10 @@ if ($pkg_fmt eq 'deb') {
 	root(qw(yum install), @pkg_opt, @pkg_install) if @pkg_install;
 } elsif ($pkg_fmt eq 'pkg_add') { # OpenBSD
 	my @pkg_opt = $opt->{yes} ? qw(-I) : (); # -I means non-interactive
-	root(qw(pkg_delete -a), @pkg_opt); # autoremove unspecified
+	root(qw(pkg_delete), @pkg_opt, @pkg_remove) if @pkg_remove;
 	@pkg_install = map { "$_--" } @pkg_install; # disambiguate w3m
 	root(qw(pkg_add), @pkg_opt, @pkg_install) if @pkg_install;
+	root(qw(pkg_delete -a), @pkg_opt) if $opt->{'allow-remove'};
 } else {
 	die "unsupported package format: $pkg_fmt\n";
 }