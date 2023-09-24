ammarfaizi2/public-inbox

commit e281363ba93763a7bc4be41ad814cd972d1775f9

lei: ensure we run DESTROY|END at daemon exit w/ kqueue
The fundamental difference which I originally missed when
implementing kqueue EVFILT_SIGNAL support is that it does not
consume signals like signalfd(2) does.  In other words, with
EVFILT_SIGNAL, it's possible for a single signal to be delivered
twice if we unblock signals upon leaving the event loop as we do
in lei.

Note: Our DS->event_loop and Sigfd APIs can/should probably be
changed to better accomodate EVFILT_SIGNAL differences from
signalfd without sacrificing usability of either.

This fixes the problem of leftover lei-ovv.dst*, lei_cfg-* and
skv.* files in $TMPDIR at the end of test suite runs on *BSD
when IO::KQueue is installed.

lib/PublicInbox/LEI.pm
@@ -1318,6 +1318,9 @@ sub lazy_start {
 		USR1 => \&noop,
 		USR2 => \&noop,
 	};
+	# for EVFILT_SIGNAL and signalfd behavioral difference:
+	my @kq_ign = eval { require PublicInbox::DSKQXS } ? keys(%$sig) : ();
+
 	require PublicInbox::DirIdle;
 	local $dir_idle = PublicInbox::DirIdle->new(sub {
 		# just rely on wakeup to hit post_loop_do
@@ -1356,13 +1359,22 @@ sub lazy_start {
 		$current_lei ? err($current_lei, @_) : warn(
 		  strftime('%Y-%m-%dT%H:%M:%SZ', gmtime(time))," $$ ", @_);
 	};
+	local $SIG{PIPE} = 'IGNORE';
 	open STDERR, '>&STDIN' or die "redirect stderr failed: $!";
 	open STDOUT, '>&STDIN' or die "redirect stdout failed: $!";
 	# $daemon pipe to `lei' closed, main loop begins:
 	eval { PublicInbox::DS::event_loop($sig, $oldset) };
 	warn "event loop error: $@\n" if $@;
+
+	# EVFILT_SIGNAL will get a duplicate of all the signals it was sent
+	local @SIG{@kq_ign} = map 'IGNORE', @kq_ign;
+	PublicInbox::DS::sig_setmask($oldset) if @kq_ign;
+
 	# exit() may trigger waitpid via various DESTROY, ensure interruptible
-	PublicInbox::DS::sig_setmask($oldset);
+	local @SIG{TERM} = sub { exit(POSIX::SIGTERM + 128) };
+	local @SIG{INT} = sub { exit(POSIX::SIGINT + 128) };
+	local @SIG{QUIT} = sub { exit(POSIX::SIGQUIT + 128) };
+	PublicInbox::DS::sig_setmask($oldset) if !@kq_ign;
 	dump_and_clear_log();
 	exit($exit_code // 0);
 }

commit 0e0f4419064a5e0b7902bd09a4e4e055bde5db70

lei: use scalar %SIG assignment
Perl v5.16.3 (and possibly some later versions) complain about
this, but newer (v5.32.1) are fine with it.

Fixes: e281363ba937 ("lei: ensure we run DESTROY|END at daemon exit w/ kqueue")

lib/PublicInbox/LEI.pm
@@ -1381,9 +1381,9 @@ sub lazy_start {
 	PublicInbox::DS::sig_setmask($oldset) if @kq_ign;
 
 	# exit() may trigger waitpid via various DESTROY, ensure interruptible
-	local @SIG{TERM} = sub { exit(POSIX::SIGTERM + 128) };
-	local @SIG{INT} = sub { exit(POSIX::SIGINT + 128) };
-	local @SIG{QUIT} = sub { exit(POSIX::SIGQUIT + 128) };
+	local $SIG{TERM} = sub { exit(POSIX::SIGTERM + 128) };
+	local $SIG{INT} = sub { exit(POSIX::SIGINT + 128) };
+	local $SIG{QUIT} = sub { exit(POSIX::SIGQUIT + 128) };
 	PublicInbox::DS::sig_setmask($oldset) if !@kq_ign;
 	dump_and_clear_log();
 	exit($exit_code // 0);