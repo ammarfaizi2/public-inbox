ammarfaizi2/public-inbox

commit ebdccd6666f97aba9285c0b337372cf13dbb3099

spawn: drop checks for directory writability
It's a TOUTTOC bug to do stat or access checks, anyways,
so just use the file and let autodie::sysopen PublicInbox::Lock
take care of the rest.

lib/PublicInbox/Spawn.pm
@@ -282,8 +282,6 @@ ALL_LIBC
 			$ENV{XDG_CACHE_HOME} //
 			( ($ENV{HOME} // '/nonexistent').'/.cache' )
 		).'/public-inbox/inline-c';
-	warn "$inline_dir exists, not writable\n" if -e $inline_dir && !-w _;
-	$all_libc = undef unless -d _ && -w _;
 	if (defined $all_libc) {
 		local $ENV{PERL_INLINE_DIRECTORY} = $inline_dir;
 		use autodie;

commit 0205e902056f9900c77840138894bf8884ea6568

spawn: reinstate directory existence check
We must not attempt to use Inline::C unless a user requests
it (by creating the directory) or running lei.

Fixes: ebdccd6666f9 (spawn: drop checks for directory writability)

lib/PublicInbox/Spawn.pm
@@ -287,6 +287,7 @@ ALL_LIBC
 			$ENV{XDG_CACHE_HOME} //
 			( ($ENV{HOME} // '/nonexistent').'/.cache' )
 		).'/public-inbox/inline-c';
+	undef $all_libc unless -d $inline_dir;
 	if (defined $all_libc) {
 		local $ENV{PERL_INLINE_DIRECTORY} = $inline_dir;
 		use autodie;