ammarfaizi2/public-inbox

commit 709fcf00c4d52a66c03190cd152f35bb9815e4e1

cindex: use run_await to read extensions.objectFormat
This saves us the trouble of seeking ourselves by using existing
run_await functionality.  We'll also be more robust to ensure we
only handle the result if the `git config' process exited without
a signal.

lib/PublicInbox/CodeSearchIdx.pm
@@ -829,20 +829,18 @@ sub prep_umask ($) {
 	}
 }
 
-sub prep_alternate_end { # awaitpid callback for config extensions.objectFormat
-	my ($pid, $objdir, $out, $run_prune) = @_;
-	my $status = $? >> 8;
+sub prep_alternate_end { # run_await cb for config extensions.objectFormat
+	my ($pid, $cmd, undef, $opt, $objdir, $run_prune) = @_;
+	my ($status, $sig) = ($? >> 8, $? & 127);
 	my $next_dir = shift(@PRUNE_QUEUE);
 	prep_alternate_start($next_dir, $run_prune) if defined($next_dir);
 	my $fmt;
-	if ($status == 1) { # unset, default is '' (SHA-1)
+	if (!$sig && $status == 1) { # unset, default is '' (SHA-1)
 		$fmt = 'sha1';
-	} elsif ($status == 0) {
-		seek($out, 0, SEEK_SET);
-		chomp($fmt = <$out> // 'sha1');
-	} else {
-		return warn("git config \$?=$? for objdir=$objdir");
+	} elsif (!$sig && $status == 0) {
+		chomp($fmt = ${$opt->{1}} || 'sha1');
 	}
+	$fmt // return warn("git config \$?=$? for objdir=$objdir");
 	my $hexlen = $OFMT2HEXLEN{$fmt} // return warn <<EOM;
 E: ignoring objdir=$objdir, unknown extensions.objectFormat=$fmt
 EOM
@@ -850,8 +848,7 @@ EOM
 		require PublicInbox::Import;
 		my $git_dir = "$TMPDIR/hexlen$hexlen.git";
 		PublicInbox::Import::init_bare($git_dir, 'cidx-all', $fmt);
-		my $f = "$git_dir/objects/info/alternates";
-		open $ALT_FH{$hexlen}, '>', $f;
+		open $ALT_FH{$hexlen}, '>', "$git_dir/objects/info/alternates";
 	}
 	say { $ALT_FH{$hexlen} } $objdir or die "say: $!";
 }
@@ -865,9 +862,7 @@ sub prep_alternate_start {
 	}
 	my $cmd = [ 'git', "--git-dir=$git_dir",
 			qw(config extensions.objectFormat) ];
-	open my $out, '+>', undef;
-	my $pid = spawn($cmd, undef, { 1 => $out });
-	awaitpid($pid, \&prep_alternate_end, $o, $out, $run_prune);
+	run_await($cmd, undef, undef, \&prep_alternate_end, $o, $run_prune);
 }
 
 sub cmd_done { # run_await cb for sort, xapian-delve, sed failures

commit 5a5d1fab7f598a707bd254ef0fd934ce9b0e25a7

cindex: quiet --prune when checking objectFormat
Most coderepos don't have extensions.objectFormat set,
so it's senseless to emit warnings on failures.

Fixes: 709fcf00c4d5 (cindex: use run_await to read extensions.objectFormat)

lib/PublicInbox/CodeSearchIdx.pm
@@ -858,7 +858,8 @@ sub prep_alternate_start {
 	}
 	my $cmd = [ 'git', "--git-dir=$git_dir",
 			qw(config extensions.objectFormat) ];
-	run_await($cmd, undef, undef, \&prep_alternate_end, $o, $run_prune);
+	my $opt = { quiet => 1 };
+	run_await($cmd, undef, $opt, \&prep_alternate_end, $o, $run_prune);
 }
 
 sub cmd_done { # run_await cb for sort, xapian-delve, sed failures

t/cindex.t
@@ -13,6 +13,7 @@ my ($tmp, $for_destroy) = tmpdir();
 my $pwd = getcwd();
 my @unused_keys = qw(last_commit has_threadid skip_docdata);
 local $ENV{PI_CONFIG} = '/dev/null';
+my $opt = { 1 => \(my $cidx_out), 2 => \(my $cidx_err) };
 
 # I reworked CodeSearchIdx->shard_worker to handle empty trees
 # in the initial commit generated by cvs2svn for xapian.git
@@ -166,7 +167,9 @@ SKIP: { # --prune
 	is(scalar($csrch->mset('s:hi')->items), 1, 'got hit');
 
 	rename("$tmp/wt0/.git", "$tmp/wt0/.giit");
-	ok(run_script([qw(-cindex -q --prune -d), "$tmp/ext"]), 'prune');
+	ok(run_script([qw(-cindex -q --prune -d), "$tmp/ext"], undef, $opt),
+		'prune');
+	is(${$opt->{2}}, '', 'nothing in stderr') or diag explain($opt);
 	$csrch->reopen;
 	is(scalar($csrch->mset('s:hi')->items), 0, 'hit pruned');
 
@@ -213,7 +216,7 @@ EOM
 	close $fh;
 	my $cmd = [ qw(-cindex -u --all --associate -d), "$tmp/ext",
 		'-I', $basic->{inboxdir} ];
-	my $opt = { 1 => \(my $cidx_out), 2 => \(my $cidx_err) };
+	$cidx_out = $cidx_err = '';
 	ok(run_script($cmd, $env, $opt), 'associate w/o search');
 	like($cidx_err, qr/W: \Q$basic->{inboxdir}\E not indexed for search/,
 		'non-Xapian-enabled inbox noted');