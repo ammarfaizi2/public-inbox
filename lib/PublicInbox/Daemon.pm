ammarfaizi2/public-inbox

commit 60d262483a4d6ddf295f5124aed3672866795ee5

daemon: use per-listener SIG{__WARN__} callbacks
This allows "-l $ADDRESS?err=/path/to/err.log to isolate normal
warn() (and carp()) messages for a particular listen address to
track down errors more easily.

lib/PublicInbox/Daemon.pm
@@ -134,6 +134,8 @@ sub load_mod ($;$$) {
 		$tlsd->{$f} = $logs{$p} //= open_log_path(my $fh, $p);
 		warn "# $scheme://$addr $f=$p\n";
 	}
+	my $err = $tlsd->{err};
+	$tlsd->{warn_cb} = sub { print $err @_ }; # for local $SIG{__WARN__}
 	\%xn;
 }
 

lib/PublicInbox/HTTP.pm
@@ -69,7 +69,7 @@ sub new ($$$) {
 
 sub event_step { # called by PublicInbox::DS
 	my ($self) = @_;
-
+	local $SIG{__WARN__} = $self->{srv_env}->{'pi-httpd.warn_cb'};
 	return unless $self->flush_write && $self->{sock};
 
 	# only read more requests if we've drained the write buffer,

lib/PublicInbox/HTTPD.pm
@@ -47,6 +47,7 @@ sub env_for ($$$) {
 		# detect when to use async paths for slow blobs
 		'pi-httpd.async' => \&pi_httpd_async,
 		'pi-httpd.app' => $self->{app},
+		'pi-httpd.warn_cb' => $self->{warn_cb},
 	}
 }
 

lib/PublicInbox/IMAP.pm
@@ -1186,7 +1186,7 @@ sub out ($$;@) {
 # callback used by PublicInbox::DS for any (e)poll (in/out/hup/err)
 sub event_step {
 	my ($self) = @_;
-
+	local $SIG{__WARN__} = $self->{imapd}->{warn_cb};
 	return unless $self->flush_write && $self->{sock} && !$self->{long_cb};
 
 	# only read more requests if we've drained the write buffer,

lib/PublicInbox/NNTP.pm
@@ -958,7 +958,7 @@ sub out ($$;@) {
 # callback used by PublicInbox::DS for any (e)poll (in/out/hup/err)
 sub event_step {
 	my ($self) = @_;
-
+	local $SIG{__WARN__} = $self->{nntpd}->{warn_cb};
 	return unless $self->flush_write && $self->{sock} && !$self->{long_cb};
 
 	# only read more requests if we've drained the write buffer,

lib/PublicInbox/POP3.pm
@@ -351,6 +351,7 @@ sub process_line ($$) {
 # callback used by PublicInbox::DS for any (e)poll (in/out/hup/err)
 sub event_step {
 	my ($self) = @_;
+	local $SIG{__WARN__} = $self->{pop3d}->{warn_cb};
 	return unless $self->flush_write && $self->{sock} && !$self->{long_cb};
 
 	# only read more requests if we've drained the write buffer,

commit 62f4880da7abf6ed6b8ae9b631ee9850e38f9e30

daemon: quiet Email::Address::XS warnings properly
Setting $SIG{__WARN__} at the top-level no longer has any effect
since we localize $SIG{__WARN__} when entering ->event_step on
a per-listener basis.

Fixes: 60d262483a4d (daemon: use per-listener SIG{__WARN__} callbacks, 2022-08-08)

lib/PublicInbox/Daemon.pm
@@ -143,8 +143,11 @@ sub load_mod ($;$$) {
 		$tlsd->{$f} = $logs{$p} //= open_log_path(my $fh, $p);
 		warn "# $scheme://$addr $f=$p\n";
 	}
+	# for per-listener $SIG{__WARN__}:
 	my $err = $tlsd->{err};
-	$tlsd->{warn_cb} = sub { print $err @_ }; # for local $SIG{__WARN__}
+	$tlsd->{warn_cb} = sub {
+		print $err @_ unless PublicInbox::Eml::warn_ignore(@_)
+	};
 	$opt->{'multi-accept'} and
 		$xn{'multi-accept'} = $opt->{'multi-accept'}->[-1];
 	\%xn;