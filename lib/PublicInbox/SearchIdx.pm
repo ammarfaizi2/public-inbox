ammarfaizi2/public-inbox

commit c25c66af0a8ac8b079babce1d127f1070267e8e6

searchidx: use vstring to improve readability
Perl has native `vstring' encoding for vector (or version)
strings, make use of it instead of relying on difficult-to-read
hex versions and integer shifts.

lib/PublicInbox/SearchIdx.pm
@@ -114,15 +114,15 @@ sub load_xapian_writable () {
 	*sortable_serialise = $xap.'::sortable_serialise';
 	$DB_CREATE_OR_OPEN = eval($xap.'::DB_CREATE_OR_OPEN()');
 	$DB_OPEN = eval($xap.'::DB_OPEN()');
-	my $ver = (eval($xap.'::major_version()') << 16) |
-		(eval($xap.'::minor_version()') << 8) |
-		eval($xap.'::revision()');
-	if ($ver >= 0x10400) {
+	my $ver = eval 'v'.join('.', eval($xap.'::major_version()'),
+				eval($xap.'::minor_version()'),
+				eval($xap.'::revision()'));
+	if ($ver ge 1.4) { # new flags in Xapian 1.4
 		$DB_NO_SYNC = 0x4;
 		$DB_DANGEROUS = 0x10;
 	}
 	# Xapian v1.2.21..v1.2.24 were missing close-on-exec on OFD locks
-	$X->{CLOEXEC_UNSET} = 1 if $ver >= 0x010215 && $ver <= 0x010218;
+	$X->{CLOEXEC_UNSET} = 1 if $ver ge v1.2.21 && $ver le v1.2.24;
 	1;
 }
 

commit b99860c560a6a4a79c73015612c855d978c3e00e

searchidx: fix Xapian version comparison
We must use Perl v-strings consistently on both sides of the
comparison.  Fortunately, this was merely a no-op and didn't
cause any data integrity problems (aside from increased wear on
the storage device).

Fixes: c25c66af0a8a (searchidx: use vstring to improve readability, 2023-04-07)

lib/PublicInbox/SearchIdx.pm
@@ -119,7 +119,7 @@ sub load_xapian_writable () {
 	my $ver = eval 'v'.join('.', eval($xap.'::major_version()'),
 				eval($xap.'::minor_version()'),
 				eval($xap.'::revision()'));
-	if ($ver ge 1.4) { # new flags in Xapian 1.4
+	if ($ver ge v1.4) { # new flags in Xapian 1.4
 		$DB_NO_SYNC = 0x4;
 		$DB_DANGEROUS = 0x10;
 	}