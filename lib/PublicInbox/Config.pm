ammarfaizi2/public-inbox

commit 0a083241373eb6d99c9900ad6ddbfe43847ac6b8

git_http_backend: use default limiter from Qspawn
There's no reason for us to have another default limiter object
when the one in Qspawn already exists.

lib/PublicInbox/GitHTTPBackend.pm
@@ -15,9 +15,6 @@ use PublicInbox::Tmpfile;
 use PublicInbox::WwwStatic qw(r @NO_CACHE);
 use Carp ();
 
-# 32 is same as the git-daemon connection limit
-my $default_limiter = PublicInbox::Limiter->new(32);
-
 # n.b. serving "description" and "cloneurl" should be innocuous enough to
 # not cause problems.  serving "config" might...
 my @text = qw[HEAD info/refs info/attributes
@@ -102,8 +99,7 @@ sub serve_smart ($$$;$) {
 		$env{$name} = $val if defined $val;
 	}
 	my $limiter = $git->{-httpbackend_limiter} //
-		($pi_cfg ? $pi_cfg->limiter('-httpbackend') : undef) //
-		$default_limiter;
+		($pi_cfg ? $pi_cfg->limiter('-httpbackend') : undef);
 	$env{GIT_HTTP_EXPORT_ALL} = '1';
 	$env{PATH_TRANSLATED} = "$git->{git_dir}/$path";
 	my $rdr = input_prepare($env) or return r(500);

commit d11b68028c6925304bb9b65b9d61802a4fe6a9d2

git_http_backend: fix 32 default connection limit
Using any configurable limiter actually defaults to `1', which
isn't intended for `-httpbackend'.  So add a $max_default
parameter to ensure we can set a per-limiter default value for
default limiters such as `-httpbackend'.

Fixes: 0a083241 (git_http_backend: use default limiter from Qspawn, 2025-01-28)

lib/PublicInbox/Config.pm
@@ -123,13 +123,13 @@ sub lookup_newsgroup {
 }
 
 sub limiter {
-	my ($self, $name) = @_;
+	my ($self, $name, $max_default) = @_;
 	$self->{-limiters}->{$name} //= do {
 		require PublicInbox::Limiter;
-		my $max = $self->{"publicinboxlimiter.$name.max"} || 1;
-		my $limiter = PublicInbox::Limiter->new($max);
-		$limiter->setup_rlimit($name, $self);
-		$limiter;
+		my $max = $self->{"publicinboxlimiter.$name.max"};
+		my $l = PublicInbox::Limiter->new($max || $max_default || 1);
+		$l->setup_rlimit($name, $self);
+		$l;
 	};
 }
 

lib/PublicInbox/GitHTTPBackend.pm
@@ -98,8 +98,9 @@ sub serve_smart ($$$;$) {
 		my $val = $env->{$name};
 		$env{$name} = $val if defined $val;
 	}
+	# 32 is the default connection limit for git-daemon, so match that
 	my $limiter = $git->{-httpbackend_limiter} //
-		($pi_cfg ? $pi_cfg->limiter('-httpbackend') : undef);
+		($pi_cfg ? $pi_cfg->limiter('-httpbackend', 32) : undef);
 	$env{GIT_HTTP_EXPORT_ALL} = '1';
 	$env{PATH_TRANSLATED} = "$git->{git_dir}/$path";
 	my $rdr = input_prepare($env) or return r(500);