ammarfaizi2/public-inbox

commit fa6a791915c45195f4c75b89855bc0b540aa7d59

xap_helper: enable FLAG_PURE_NOT in external process
Since public-facing WWW uses an external process with
a non-blocking socket, clients will only see 503 errors
if the search is overloaded.  While allowing pure NOT
queries is expensive, there are already many possible
ways of triggering expensive queries so it's probably not
a problem to allow one more.

lib/PublicInbox/XapHelper.pm
@@ -20,6 +20,7 @@ use Carp qw(croak);
 my $X = \%PublicInbox::Search::X;
 our (%SRCH, %WORKERS, $nworker, $workerset, $in, $SHARD_NFD, $MY_FD_MAX);
 our $stderr = \*STDERR;
+my $QP_FLAGS = $PublicInbox::Search::QP_FLAGS;
 
 sub cmd_test_inspect {
 	my ($req) = @_;
@@ -194,7 +195,7 @@ sub dispatch {
 	$key .= "\0".join("\0", map { ('-Q', $_) } @{$req->{Q}}) if $req->{Q};
 	my $new;
 	$req->{srch} = $SRCH{$key} // do {
-		$new = { qp_flags => $PublicInbox::Search::QP_FLAGS };
+		$new = { qp_flags => $QP_FLAGS };
 		my $nfd = scalar(@$dirs) * PublicInbox::Search::SHARD_COST;
 		$SHARD_NFD += $nfd;
 		if ($SHARD_NFD > $MY_FD_MAX) {
@@ -337,6 +338,7 @@ sub start (@) {
 		die "E: unable to get RLIMIT_NOFILE: $!";
 	warn "W: RLIMIT_NOFILE=$MY_FD_MAX too low\n" if $MY_FD_MAX < 72;
 	$MY_FD_MAX -= 64;
+	$QP_FLAGS |= PublicInbox::Search::FLAG_PURE_NOT();
 
 	local $nworker = $opt->{j};
 	return recv_loop() if $nworker == 0;

lib/PublicInbox/xap_helper.h
@@ -590,6 +590,7 @@ static void srch_init(struct req *req)
 	const unsigned FLAG_PHRASE = Xapian::QueryParser::FLAG_PHRASE;
 	srch->qp_flags = Xapian::QueryParser::FLAG_BOOLEAN |
 			Xapian::QueryParser::FLAG_LOVEHATE |
+			Xapian::QueryParser::FLAG_PURE_NOT |
 			Xapian::QueryParser::FLAG_WILDCARD;
 	long nfd = req->dirc * SHARD_COST;
 

commit e7add9d3873c62b70ceec520cdfd000e191ba9e1

XapHelper.pm: fix QP_FLAGS initialization
We can't use $PublicInbox::Search::QP_FLAGS until after
load_xapian is called.  Failure to set the correct query parser
flags was causing failures in
`PI_NO_CXX=1 TEST_DAEMON_XH=-X0 prove -bw t/imapd.t'
since phrase parsing was broken with the Perl bindings
XapHelper implementation.

Now, the `check-xh0' and `check-xh1' targets both pass with
PI_NO_CXX=1 set to disable the C++ xap_helper.

Fixes: fa6a7919 (xap_helper: enable FLAG_PURE_NOT in external process, 2025-02-23)

lib/PublicInbox/XapHelper.pm
@@ -20,7 +20,7 @@ use Carp qw(croak);
 my $X = \%PublicInbox::Search::X;
 our (%SRCH, %WORKERS, $nworker, $workerset, $in, $SHARD_NFD, $MY_FD_MAX);
 our $stderr = \*STDERR;
-my $QP_FLAGS = $PublicInbox::Search::QP_FLAGS;
+my $QP_FLAGS;
 
 sub cmd_test_inspect {
 	my ($req) = @_;
@@ -338,6 +338,8 @@ sub start (@) {
 
 	local (%SRCH, %WORKERS, $SHARD_NFD, $MY_FD_MAX);
 	PublicInbox::Search::load_xapian();
+	$QP_FLAGS = $PublicInbox::Search::QP_FLAGS |
+		PublicInbox::Search::FLAG_PURE_NOT();
 	$GLP->getoptionsfromarray(\@argv, my $opt = { j => 1 }, 'j=i') or
 		die 'bad args';
 	local $workerset = POSIX::SigSet->new;
@@ -349,7 +351,6 @@ sub start (@) {
 		die "E: unable to get RLIMIT_NOFILE: $!";
 	warn "W: RLIMIT_NOFILE=$MY_FD_MAX too low\n" if $MY_FD_MAX < 72;
 	$MY_FD_MAX -= 64;
-	$QP_FLAGS |= PublicInbox::Search::FLAG_PURE_NOT();
 
 	local $nworker = $opt->{j};
 	return recv_loop() if $nworker == 0;