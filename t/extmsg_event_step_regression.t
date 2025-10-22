ammarfaizi2/public-inbox

commit c3d0295bd96297600a98b1d6aa6d79790518d906

www: extmsg: async partial Message-ID search
While partial Message-ID matches tend to be relatively fast,
there's still some potential for pathological matches taking
a long time and blocking our event loop.  So throw them over
to the external xap_helper process to ensure our main event
loop can handle other HTTP/NNTP/POP3/IMAP requests while
potentially waiting on Xapian disk seeks.

lib/PublicInbox/ExtMsg.pm
@@ -29,11 +29,30 @@ our @EXT_URL = map { ascii_html($_) } (
 
 sub PARTIAL_MAX () { 100 }
 
-sub search_partial ($$) {
-	my ($ibx, $mid) = @_;
+sub partial_cb { # async_mset cb
+	my ($ctx, $srch, $mset, $err) = @_;
+	if ($err) {
+		my $msg = "W: query failed: $! ($err)";
+		++$ctx->{ext_msg_partial_fail};
+		warn($msg);
+	} else {
+		my $ibx = $ctx->{partial_ibx};
+		my @mid = map { $_->{mid} } @{$srch->mset_to_smsg($ibx, $mset)};
+		if (scalar @mid) {
+			push @{$ctx->{partial}}, [ $ibx, \@mid ];
+			(($ctx->{n_partial} += scalar(@mid)) >= PARTIAL_MAX) and
+				delete $ctx->{again}; # done
+		}
+	}
+	PublicInbox::DS::requeue($ctx) if $ctx->{env}->{'pi-httpd.app'};
+}
+
+# returns true if started asynchronously
+sub partial_ibx_start ($$) {
+	my ($ctx, $ibx) = @_;
+	my $mid = $ctx->{mid};
 	return if length($mid) < $MIN_PARTIAL_LEN;
 	my $srch = $ibx->isrch or return;
-	my $opt = { limit => PARTIAL_MAX, sort_col => -1, asc => 1 };
 	my @try = ("m:$mid*");
 	my $chop = $mid;
 	if ($chop =~ s/(\W+)(\w*)\z//) {
@@ -59,18 +78,9 @@ sub search_partial ($$) {
 			push(@try, join(' ', map { "m:$_" } @long));
 		}
 	}
-
-	foreach my $m (@try) {
-		# If Xapian can't handle the wildcard since it
-		# has too many results.  $@ can be
-		# Search::Xapian::QueryParserError or even:
-		# "something terrible happened at ../Search/Xapian/Enquire.pm"
-		my $mset = eval { $srch->mset($m, $opt) } or next;
-		my @mids = map {
-			$_->{mid}
-		} @{$srch->mset_to_smsg($ibx, $mset)};
-		return \@mids if scalar(@mids);
-	}
+	$ctx->{partial_ibx} = $ibx;
+	my $opt = { limit => PARTIAL_MAX, sort_col => -1, asc => 1 };
+	$srch->async_mset(\@try, $opt, \&partial_cb, $ctx, $srch);
 }
 
 sub ext_msg_i {
@@ -104,19 +114,28 @@ sub ext_msg_step {
 	}
 }
 
-sub try_partial ($) {
+sub partial_enter ($) {
 	my ($ctx) = @_;
 	bless $ctx, __PACKAGE__; # for ExtMsg->event_step
 	return $ctx->event_step if $ctx->{env}->{'pi-httpd.app'};
 	$ctx->event_step(1) while $ctx->{-wcb};
 }
 
+sub partial_prepare ($@) {
+	my ($ctx, @try_ibxish) = @_;
+	$ctx->{again} = \@try_ibxish;
+	sub {
+		$ctx->{-wcb} = $_[0]; # HTTP server write callback
+		partial_enter $ctx;
+	}
+}
+
 sub ext_msg_ALL ($) {
 	my ($ctx) = @_;
 	my $ALL = $ctx->{www}->{pi_cfg}->ALL or return;
+	return partial_prepare($ctx, $ALL) if $ALL == $ctx->{ibx};
 	my $by_eidx_key = $ctx->{www}->{pi_cfg}->{-by_eidx_key};
-	my $cur_key = eval { $ctx->{ibx}->eidx_key } //
-			return partial_response($ctx); # $cur->{ibx} == $ALL
+	my $cur_key = $ctx->{ibx}->eidx_key;
 	my %seen = ($cur_key => 1);
 	my ($id, $prev);
 	while (my $x = $ALL->over->next_by_mid($ctx->{mid}, \$id, \$prev)) {
@@ -129,14 +148,7 @@ sub ext_msg_ALL ($) {
 			push(@{$ctx->{found}}, $ibx) unless $seen{$k}++;
 		}
 	}
-	return exact($ctx) if $ctx->{found};
-
-	# fall back to partial MID matching
-	$ctx->{again} = [ $ctx->{ibx}, $ALL ];
-	sub {
-		$ctx->{-wcb} = $_[0]; # HTTP server write callback
-		try_partial $ctx;
-	}
+	$ctx->{found} ? exact($ctx) : partial_prepare($ctx, $ctx->{ibx}, $ALL);
 }
 
 # only public entry point
@@ -165,30 +177,19 @@ sub event_step {
 	my ($ctx, $sync) = @_;
 	# can't find a partial match in current inbox, try the others:
 	my $ibx = shift @{$ctx->{again}} or return finalize_partial($ctx);
-	my $mids = search_partial($ibx, $ctx->{mid}) or
-			return ($sync ? undef : PublicInbox::DS::requeue($ctx));
-	$ctx->{n_partial} += scalar(@$mids);
-	push @{$ctx->{partial}}, [ $ibx, $mids ];
-	$ctx->{n_partial} >= PARTIAL_MAX ? finalize_partial($ctx)
-			: ($sync ? undef : PublicInbox::DS::requeue($ctx));
+	unless (partial_ibx_start($ctx, $ibx)) {
+		PublicInbox::DS::requeue($ctx) unless $sync;
+	}
 }
 
 sub finalize_exact {
 	my ($ctx) = @_;
-
-	return delete($ctx->{-wcb})->(exact($ctx)) if $ctx->{found};
-
-	# fall back to partial MID matching
-	my $mid = $ctx->{mid};
-	my $cur = $ctx->{ibx};
-	my $mids = search_partial($cur, $mid);
-	if ($mids) {
-		$ctx->{n_partial} = scalar(@$mids);
-		push @{$ctx->{partial}}, [ $cur, $mids ];
-	} elsif ($ctx->{again} && length($mid) >= $MIN_PARTIAL_LEN) {
-		return try_partial $ctx;
+	if ($ctx->{found}) {
+		delete($ctx->{-wcb})->(exact($ctx));
+	} else { # no exact matches? fall back to partial msgid matching
+		$ctx->{again} = [ $ctx->{ibx} ];
+		partial_enter $ctx;
 	}
-	finalize_partial($ctx);
 }
 
 sub _url_pfx ($$;$) {
@@ -230,6 +231,13 @@ sub partial_response ($) {
 		$s .= $ext;
 		$code = 300;
 	}
+	if (my $nr = delete $ctx->{ext_msg_partial_fail}) {
+		$s .= <<EOM
+
+$nr internal search queries failed (likely due to server overload)
+EOM
+	}
+	chop $s; # omit trailing \n
 	$ctx->{-html_tip} = $s .= '</pre>';
 	$ctx->{-title_html} = $title;
 	html_oneshot($ctx, $code);

lib/PublicInbox/Isearch.pm
@@ -72,10 +72,10 @@ sub mset {
 }
 
 sub async_mset {
-	my ($self, $str, $opt, $cb, @args) = @_;
+	my ($self, $qry, $opt, $cb, @args) = @_;
 	$opt = eidx_mset_prep $self, $opt;
 	local $self->{es}->{-extra} = $self->{-extra} if $self->{-extra};
-	$self->{es}->async_mset($str, $opt, $cb, @args);
+	$self->{es}->async_mset($qry, $opt, $cb, @args);
 }
 
 sub mset_to_artnums {

lib/PublicInbox/LeiRemote.pm
@@ -58,8 +58,13 @@ sub mset {
 
 # fake support for async API
 sub async_mset {
-	my ($self, $qstr, undef, $cb, @arg) = @_; # $opt ($_[2]) ignored
-	$cb->(@arg, mset($self, $qstr));
+	my ($self, $qry, undef, $cb, @arg) = @_; # $opt ($_[2]) ignored
+	my $mset;
+	for my $qstr (ref($qry) eq 'ARRAY' ? @$qry : ($qry)) {
+		$mset = eval { mset($self, $qstr) };
+		last if $mset && $mset->size;
+	}
+	$mset ? $cb->(@arg, $mset) : $cb->(@arg, undef, $@ || 'err');
 	undef;
 }
 

lib/PublicInbox/Search.pm
@@ -482,15 +482,15 @@ sub xh_opt ($$) {
 # returns a true value if actually handled asynchronously,
 # and a falsy value if handled synchronously
 sub async_mset {
-	my ($self, $qry_str, $opt, $cb, @args) = @_;
+	my ($self, $qry, $opt, $cb, @args) = @_;
 	if ($XHC) { # unconditionally retrieving pct + rank for now
 		xdb($self); # populate {nshards}
 		my @margs = ($self->xh_args, xh_opt($self, $opt), '--');
 		my $ret = eval {
 			pipe my $out_rd, my $out_wr;
 			open my $err_rw, '+>', undef;
-			$XHC->mkreq([ $out_wr, $err_rw ],
-					'mset', @margs, $qry_str);
+			$XHC->mkreq([ $out_wr, $err_rw ], 'mset', @margs,
+				(ref($qry) eq 'ARRAY' ? @$qry : $qry));
 			undef $out_wr;
 			PublicInbox::XhcMset->maybe_new($out_rd, $err_rw,
 							$self, $cb, @args);
@@ -498,8 +498,12 @@ sub async_mset {
 		$cb->(@args, undef, $@) if $@;
 		$ret;
 	} else { # synchronous
-		my $mset = eval { $self->mset($qry_str, $opt) };
-		$@ ? $cb->(@args, undef, $@) : $cb->(@args, $mset);
+		my $mset;
+		for my $qstr (ref($qry) eq 'ARRAY' ? @$qry : ($qry)) {
+			$mset = eval { $self->mset($qstr, $opt) };
+			last if $mset && $mset->size;
+		}
+		$mset ? $cb->(@args, $mset) : $cb->(@args, undef, $@ || 'err');
 		undef;
 	}
 }

commit 18420b5f359def1afc569d3a42aab711cfc99acb

extmsg: fix Message-ID search for non-xap_helper users
commit c3d0295bd was intended to avoid head-of-line blocking for
--xapian-helpers (-X) deployments.  Unfortunately, that broke
searches for deployments not using --xapian-helpers by causing
the ExtMsg to get queued redundantly into the event loop.  So
perform the `requeue' procedure exactly once and simplify
ExtMsg->event_step by eliminating the $sync parameter.

Fixes: c3d0295bd (www: extmsg: async partial Message-ID search, 2025-05-12)
Reported-by: Kyle Meyer <kyle@kyleam.com>
Link: https://public-inbox.org/meta/877bwva3s4.fsf@kyleam.com/

lib/PublicInbox/ExtMsg.pm
@@ -137,7 +137,7 @@ sub partial_enter ($) {
 	my ($ctx) = @_;
 	bless $ctx, __PACKAGE__; # for ExtMsg->event_step
 	return $ctx->event_step if $ctx->{env}->{'pi-httpd.app'};
-	$ctx->event_step(1) while $ctx->{-wcb};
+	$ctx->event_step while $ctx->{-wcb}; # generic PSGI
 }
 
 sub partial_prepare ($@) {
@@ -193,12 +193,10 @@ sub ext_msg {
 
 # called via PublicInbox::DS::event_loop
 sub event_step {
-	my ($ctx, $sync) = @_;
+	my ($ctx) = @_;
 	# can't find a partial match in current inbox, try the others:
 	my $ibx = shift @{$ctx->{again}} or return finalize_partial($ctx);
-	unless (partial_ibx_start($ctx, $ibx)) {
-		PublicInbox::DS::requeue($ctx) unless $sync;
-	}
+	partial_ibx_start $ctx, $ibx;
 }
 
 sub finalize_exact {

t/extmsg_event_step_regression.t
@@ -1,0 +1,70 @@
+#!perl -w
+# Copyright (C) all contributors <meta@public-inbox.org>
+# License: AGPL-3.0+ <https://www.gnu.org/licenses/agpl-3.0.txt>
+use v5.12;
+use PublicInbox::TestCommon;
+use PublicInbox::Eml;
+use PublicInbox::IO qw(write_file);
+require_mods qw(-httpd v2 psgi);
+my $tmpdir = tmpdir;
+my $one = PublicInbox::Eml->new(<<'EOM');
+From: X <x@example.com>
+Subject: subj
+Date: Sat, 16 Jun 2007 17:33:53 +0100
+Message-ID: <681fb71bcf6@example.com>
+To: list@example.com
+Mime-Version: 1.0
+Content-Type: text/plain
+
+abc
+EOM
+my $two = PublicInbox::Eml->new(<<'EOM');
+From: X <x@example.com>
+Subject: Re: subj
+Date: Sat, 30 Jun 2007 18:56:36 +0100
+Message-ID: <eb1c75afc92@example.com>
+References: <681fb71bcf6@example.com>
+    <de34bbcfcf7@example.com>
+In-Reply-To: <de34bbcfcf7@example.com>
+To: Y <y@example.com>
+Cc: list@example.com
+MIME-Version: 1.0
+Content-Type: text/plain
+
+def
+EOM
+my %ibx = map {
+	$_ => create_inbox "v$_-event_step", version => $_,
+			indexlevel => 'medium', sub {
+		my ($im, $ibx) = @_;
+		$im->add($one) or xbail 'add 1';
+		$im->add($two) or xbail 'add 2';
+	};
+} (1, 2);
+write_file '>', my $cfgpath = "$tmpdir/config", <<EOM;
+[publicinbox "list"]
+	address = list\@example.com
+        url = http://example.com/list
+	inboxdir = $ibx{1}->{inboxdir}
+[publicinbox "list2"]
+	address = list2\@example.com
+        url = http://example.com/list2
+	inboxdir = $ibx{2}->{inboxdir}
+EOM
+my $env = { TMPDIR => $tmpdir, PI_CONFIG => $cfgpath };
+my $client = sub {
+	my ($cb) = @_;
+	my $res = $cb->(GET('/list/de34bbcfcf7@example.com/'));
+	is $res->code, 300, 'v1 300';
+	$res = $cb->(GET('/list2/de34bbcfcf7@example.com/'));
+	is $res->code, 300, 'v2 300';
+};
+
+test_httpd($env, $client);
+{
+	require PublicInbox::WWW;
+	my $cfg = PublicInbox::Config->new($cfgpath);
+	my $www = PublicInbox::WWW->new($cfg);
+	test_psgi(sub { $www->call(@_) }, $client);
+}
+done_testing;